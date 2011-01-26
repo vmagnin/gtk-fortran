d1e8d42f9d09150e4081708de44c72ff9c2cca9d
jerry <jvdelisle@frontier.com>
2011-01-25 18:11:48 -0800

    2011-01-25  Jerry DeLisle  <jvdelisle@gcc.gnu.org>
    	* src/cairo.f90: Fix indentation and add interface block.

--- src/cairo.f90
+++ src/cairo.f90
@@ -35,61 +35,63 @@ module cairo
   integer(c_int), parameter :: CAIRO_FORMAT_A1        = 3
   integer(c_int), parameter :: CAIRO_FORMAT_RGB16_565 = 4
 
-  function gdk_cairo_create (drawable) bind(c)
-    use iso_c_binding, only: c_ptr
-    type(c_ptr), value :: drawable
-    type(c_ptr) :: gdk_cairo_create
-  end function
-
-  function cairo_create (target0) bind(c)
-    use iso_c_binding, only: c_ptr
-    type(c_ptr), value :: target0
-    type(c_ptr) :: cairo_create
-  end function
-
-  function cairo_image_surface_create (format0, width, height) bind(c)
-    use iso_c_binding, only: c_ptr, c_int
-    integer(c_int), value :: format0, width, height
-    type(c_ptr) :: cairo_image_surface_create
-  end function
+  interface
+    function gdk_cairo_create (drawable) bind(c)
+      use iso_c_binding, only: c_ptr
+      type(c_ptr), value :: drawable
+      type(c_ptr) :: gdk_cairo_create
+    end function
   
-  subroutine cairo_destroy(cr) bind(c)
-    use iso_c_binding, only: c_ptr
-    type(c_ptr), value :: cr
-  end subroutine 
-
-  subroutine cairo_stroke(cr) bind(c)
-    use iso_c_binding, only: c_ptr
-    type(c_ptr), value :: cr
-  end subroutine 
-
-  subroutine cairo_move_to(cr, x, y) bind(c)
-    use iso_c_binding, only: c_ptr, c_double
-    type(c_ptr), value :: cr
-    real(c_double), value :: x, y
-  end subroutine 
+    function cairo_create (target0) bind(c)
+      use iso_c_binding, only: c_ptr
+      type(c_ptr), value :: target0
+      type(c_ptr) :: cairo_create
+    end function
   
-  subroutine cairo_line_to(cr, x, y) bind(c)
-    use iso_c_binding, only: c_ptr, c_double
-    type(c_ptr), value :: cr
-    real(c_double), value :: x, y
-  end subroutine 
-
-  subroutine cairo_curve_to(cr, x1, y1, x2, y2, x3, y3) bind(c)
-    use iso_c_binding, only: c_ptr, c_double
-    type(c_ptr), value :: cr
-    real(c_double), value :: x1, y1, x2, y2, x3, y3
-  end subroutine 
+    function cairo_image_surface_create (format0, width, height) bind(c)
+      use iso_c_binding, only: c_ptr, c_int
+      integer(c_int), value :: format0, width, height
+      type(c_ptr) :: cairo_image_surface_create
+    end function
+    
+    subroutine cairo_destroy(cr) bind(c)
+      use iso_c_binding, only: c_ptr
+      type(c_ptr), value :: cr
+    end subroutine 
   
-  subroutine cairo_set_source_rgb(cr, red, green, blue) bind(c)
-    use iso_c_binding, only: c_ptr, c_double
-    type(c_ptr), value :: cr
-    real(c_double), value :: red, green, blue
-  end subroutine 
-
-  subroutine cairo_set_line_width(cr, width) bind(c)
-    use iso_c_binding, only: c_ptr, c_double
-    type(c_ptr), value :: cr
-    real(c_double), value :: width
-  end subroutine 
+    subroutine cairo_stroke(cr) bind(c)
+      use iso_c_binding, only: c_ptr
+      type(c_ptr), value :: cr
+    end subroutine 
+  
+    subroutine cairo_move_to(cr, x, y) bind(c)
+      use iso_c_binding, only: c_ptr, c_double
+      type(c_ptr), value :: cr
+      real(c_double), value :: x, y
+    end subroutine 
+    
+    subroutine cairo_line_to(cr, x, y) bind(c)
+      use iso_c_binding, only: c_ptr, c_double
+      type(c_ptr), value :: cr
+      real(c_double), value :: x, y
+    end subroutine 
+  
+    subroutine cairo_curve_to(cr, x1, y1, x2, y2, x3, y3) bind(c)
+      use iso_c_binding, only: c_ptr, c_double
+      type(c_ptr), value :: cr
+      real(c_double), value :: x1, y1, x2, y2, x3, y3
+    end subroutine 
+    
+    subroutine cairo_set_source_rgb(cr, red, green, blue) bind(c)
+      use iso_c_binding, only: c_ptr, c_double
+      type(c_ptr), value :: cr
+      real(c_double), value :: red, green, blue
+    end subroutine 
+  
+    subroutine cairo_set_line_width(cr, width) bind(c)
+      use iso_c_binding, only: c_ptr, c_double
+      type(c_ptr), value :: cr
+      real(c_double), value :: width
+    end subroutine 
+  end interface
 end module cairo
