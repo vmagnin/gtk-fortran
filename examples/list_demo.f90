f49ef1a05c366160067dc1525ad3ff205cbc4227
Vincent Magnin <vincent.magnin@libertysurf.fr>
2011-03-22 17:57:43 +0100

    * examples/list_demo.f90: a GtkTreeView demo contributed by James Tappin. * src/gtk-sup.f90: supplementary GTK parameters and functions contributed by James Tappin.

---
+++ examples/list_demo.f90
@@ -0,0 +1,113 @@
+program list_demo
+  ! A translation into Fortran of the "hello_world" tree demo from
+  ! the GTK TreeView Tutorial.
+  ! Original version by: Tim-Philipp Müller (2005)
+  ! http://scentric.net/tutorial/ch-treeview.html#sec-TreeView-HelloWorld
+  ! varargs calls replaced & translated into Fortran: James Tappin
+  ! 21-Mar-2011
+
+  use gtk
+  use fgtk_utils  ! Contains iter structure and gtypes definitions
+
+  enum, bind(c)
+     enumerator :: COL_NAME = 0
+     enumerator :: COL_AGE
+     enumerator :: NUM_COLS
+  end enum
+
+  type(c_ptr) :: window, view
+
+  call gtk_init()
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL)
+  call g_signal_connect (window, "delete_event"//CNULL, &
+       & c_funloc(gtk_main_quit), NULL); ! dirty
+
+  view = create_view_and_model ()
+
+  call gtk_container_add (window, view)
+
+  call gtk_widget_show_all (window)
+
+  call gtk_main ()
+
+contains
+  function create_and_fill_model() result(store)
+    type(c_ptr) :: store
+
+    integer(kind=type_kind), dimension(2), target :: ctypes = &
+         & [g_type_string, g_type_uint]
+    type(gtktreeiter), target :: iter
+    type(gvalue), target :: valt, vali
+    type(c_ptr) :: val
+
+    ! Initialize the GValues
+    val = c_loc(valt)
+    val = g_value_init(val, G_TYPE_STRING)
+    val = c_loc(vali)
+    val = g_value_init(val, G_TYPE_UINT)
+
+    ! Create the list store
+
+    store = gtk_list_store_newv(NUM_COLS, c_loc(ctypes))
+
+    ! Append row 1 and add data
+
+    call gtk_list_store_append(store, c_loc(iter))
+    call g_value_set_static_string(c_loc(valt), "Heinz El-Mann"//CNULL)
+    call gtk_list_store_set_value(store, c_loc(iter), COL_NAME, &
+         & c_loc(valt))
+    call g_value_set_uint(c_loc(vali), 51)
+    call gtk_list_store_set_value(store, c_loc(iter), COL_AGE, c_loc(vali))
+
+    ! append another row and fill in some data
+    call gtk_list_store_append (store, c_loc(iter))
+    call g_value_set_static_string(c_loc(valt), "Jane Doe"//CNULL)
+    call gtk_list_store_set_value(store, c_loc(iter), COL_NAME, c_loc(valt))
+    call g_value_set_uint(c_loc(vali), 23)
+    call gtk_list_store_set_value(store, c_loc(iter), COL_AGE, c_loc(vali))
+
+    ! And a third
+    call gtk_list_store_append (store, c_loc(iter))
+    call g_value_set_static_string(c_loc(valt), "Joe Bungop"//cnull)
+    call gtk_list_store_set_value(store, c_loc(iter), COL_NAME, c_loc(valt))
+    call g_value_set_uint(c_loc(vali), 91)
+    call gtk_list_store_set_value(store, c_loc(iter), COL_AGE, c_loc(vali))
+
+  end function create_and_fill_model
+
+  function create_view_and_model() result(view)
+    type(c_ptr) :: view
+
+    type(c_ptr) :: col, renderer, model
+    integer(kind=c_int) :: ncol
+
+    view = gtk_tree_view_new ()
+
+    ! --- Column #1 ---
+
+    renderer = gtk_cell_renderer_text_new ()
+    col = gtk_tree_view_column_new()
+    call gtk_tree_view_column_pack_start(col, renderer, FALSE)
+    ncol= gtk_tree_view_insert_column(view, col, -1)
+    call gtk_tree_view_column_add_attribute(col, renderer, &
+         & "text"//cnull, COL_NAME)
+    call gtk_tree_view_column_set_title(col, "Name"//cnull)
+
+
+    ! --- Column #2 ---
+
+    col = gtk_tree_view_column_new()
+    renderer = gtk_cell_renderer_text_new ()
+    call gtk_tree_view_column_pack_start(col, renderer, FALSE)
+    ncol = gtk_tree_view_insert_column(view, col, -1)
+    call gtk_tree_view_column_set_title(col, "Age"//cnull)
+    call gtk_tree_view_column_add_attribute(col, renderer, &
+         & "text"//cnull, COL_AGE)
+
+    model = create_and_fill_model ()
+
+    call gtk_tree_view_set_model (view, model)
+
+    call g_object_unref (model)  ! destroy model automatically with view
+  end function create_view_and_model
+end program list_demo
---
+++ src/gtk-sup.f90
@@ -0,0 +1,120 @@
+! Copyright (C) 2011
+! Free Software Foundation, Inc.
+
+! This file is part of the gtk-fortran GTK+ Fortran Interface library.
+
+! This is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 3, or (at your option)
+! any later version.
+
+! This software is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+
+! Under Section 7 of GPL version 3, you are granted additional
+! permissions described in the GCC Runtime Library Exception, version
+! 3.1, as published by the Free Software Foundation.
+
+! You should have received a copy of the GNU General Public
+!  License along with
+! this program; see the files COPYING3 and COPYING.RUNTIME respectively.
+! If not, see <http://www.gnu.org/licenses/>.
+!
+! Contributed by James Tappin
+! Last modification: 03-21-2011
+
+module fgtk_utils
+  ! The definitions in this supplementary module are ones that are not (yet)
+  !  automatically extracted from the GTK headers.
+
+  ! Currently it contains:
+  ! GTYPE: Definitions of the integer length and the values for each type.
+  ! GtkTreeIter: Type definition.
+  ! GValue: Pseudo type definition.
+
+  use iso_c_binding
+  use gtk
+
+  implicit none
+
+  ! Gtype definitions
+  integer, parameter :: type_kind=c_long
+  integer(kind=c_int), parameter :: g_type_fundamental_shift=2
+  integer(kind=type_kind), parameter :: G_TYPE_INVALID = &
+       & ishft(0_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_NONE = &
+       & ishft(1_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_INTERFACE = &
+       & ishft(2_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_CHAR = &
+       & ishft(3_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_UCHAR = &
+       & ishft(4_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_BOOLEAN = &
+       & ishft(5_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_INT = &
+       & ishft(6_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_UINT = &
+       & ishft(7_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_LONG = &
+       & ishft(8_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_ULONG = &
+       & ishft(9_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_INT64 = &
+       & ishft(10_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_UINT64 = &
+       & ishft(11_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_ENUM = &
+       & ishft(12_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_FLAGS = &
+       & ishft(13_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_FLOAT = &
+       & ishft(14_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_DOUBLE = &
+       & ishft(15_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_STRING = &
+       & ishft(16_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_POINTER = &
+       & ishft(17_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_BOXED = &
+       & ishft(18_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_PARAM = &
+       & ishft(19_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_OBJECT = &
+       & ishft(20_type_kind, g_type_fundamental_shift)
+  integer(kind=type_kind), parameter :: G_TYPE_VARIANT = &
+       & ishft(21_type_kind, g_type_fundamental_shift)
+
+
+  ! Define a GtkTreeIter (this has to be pre-allocated in the calls)
+  type, bind(c) :: gtktreeiter
+     integer(kind=c_int) :: intv=0
+     type(c_ptr) :: p0=NULL, p1=NULL, p2=NULL
+  end type gtktreeiter
+
+  ! Define a spacemaker for GValue It's 24 bytes on 64 bit & 20 on 32,
+  ! i.e. one long and 2 64-bit integers
+  type, bind(c) :: gvalue
+     integer(kind=c_long) :: il = 0
+     integer(kind=c_int64_t), dimension(2) :: i64 = (/0, 0/)
+  end type gvalue
+
+contains
+  ! These 2 clear_ routines are only needed of you need to re-initialize
+  ! the types. The definitions include the intial setting to zero or NULL.
+  subroutine clear_gtktreeiter(iter)
+    type(gtktreeiter), intent(inout) :: iter
+
+    iter%intv=0
+    iter%p0=NULL
+    iter%p1=NULL
+    iter%p2=NULL
+  end subroutine clear_gtktreeiter
+  subroutine clear_gvalue(gval)
+    type(gvalue) :: gval
+    gval%il=0
+    gval%i64=(/0,0/)
+  end subroutine clear_gvalue
+end module fgtk_utils
