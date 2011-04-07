563aec93231c39d6f0af2353ea0debb9df2e7f1e
vmagnin <vincent.magnin@libertysurf.fr>
2011-04-07 22:11:06 +0200

    * src/test.sh: compiles and runs all gtk-fortran files and examples.

--- src/test.sh
+++ src/test.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
-# Compilation of the examples, for testing gtk-fortran
+# Compilation of gtk-fortran and its examples
+# April 7th 2011
 
-echo "gtk"
+echo "gtk..."
 gfortran -c gdk-auto.f90 `pkg-config --cflags --libs gtk+-2.0`
 gfortran -c glib-auto.f90 `pkg-config --cflags --libs gtk+-2.0`
 gfortran -c gtk.f90 `pkg-config --cflags --libs gtk+-2.0`
@@ -9,7 +10,13 @@ gfortran -c atk-auto.f90 `pkg-config --cflags --libs gtk+-2.0`
 gfortran -c cairo-auto.f90 `pkg-config --cflags --libs gtk+-2.0`
 gfortran -c gdk-pixbuf-auto.f90 `pkg-config --cflags --libs gtk+-2.0`
 gfortran -c pango-auto.f90 `pkg-config --cflags --libs gtk+-2.0`
+gfortran -c gtk-sup.f90 `pkg-config --cflags --libs gtk+-2.0`
+gfortran -c gtk-hl.f90 `pkg-config --cflags --libs gtk+-2.0`
 
-for i in `ls -tr ../examples/*.f90` ; do echo $i ; gfortran *.o $i  `pkg-config --cflags --libs gtk+-2.0` -o $i.out ; done
+echo "examples..."
+for i in `ls -tr ../examples/*.f90` ; do echo $i ; gfortran gtk.o gtk-sup.o gtk-hl.o $i  `pkg-config --cflags --libs gtk+-2.0` -o $i.out ; done
+gfortran gtk.o ../examples/gtkbuilder2.f90 -o ../examples/gtkbuilder2.f90.out `pkg-config --cflags --libs gtk+-2.0` `pkg-config --cflags --libs gmodule-2.0`
 
-for i in `ls -tr ../examples/*.out` ; do $i ; done
+cd ../examples/
+
+for i in `ls -tr *.out` ; do ./$i ; done
