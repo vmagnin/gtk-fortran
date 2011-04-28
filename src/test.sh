e6a0fc6ece4ae6e72b5ef95621c7997119ad432c
vmagnin <vincent.magnin@libertysurf.fr>
2011-04-28 22:58:41 +0200

    test.sh: code cleaning

--- src/test.sh
+++ src/test.sh
@@ -2,29 +2,27 @@
 # Compilation of gtk-fortran and its examples, using GTK+ 3.0
 # April 8th 2011
 
+echo "Removing old files..."
 rm *.o
 rm *.mod
 rm *.out
 rm ../examples/*.mod
 rm ../examples/*.out
 
+gtkversion="`pkg-config --cflags --libs gtk+-3.0`"
+
 echo "gtk..."
-gfortran -c gdk-auto.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c glib-auto.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c gtk.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c unix-print-auto.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c atk-auto.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c cairo-auto.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c gdk-pixbuf-auto.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c pango-auto.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c gtk-sup.f90 `pkg-config --cflags --libs gtk+-3.0`
-gfortran -c gtk-hl.f90 `pkg-config --cflags --libs gtk+-3.0`
+for file in "gdk-auto.f90" "glib-auto.f90" "gtk.f90" "unix-print-auto.f90" "atk-auto.f90" "cairo-auto.f90" "gdk-pixbuf-auto.f90" "pango-auto.f90" "gtk-sup.f90" "gtk-hl.f90"; do 
+  gfortran -c $file $gtkversion
+done
 
 echo "examples..."
-for i in `ls -tr ../examples/*.f90` ; do echo $i ; gfortran gtk.o gtk-sup.o gtk-hl.o $i  `pkg-config --cflags --libs gtk+-3.0` -o $i.out ; done
-#for i in `ls -tr ../examples/*.f90` ; do echo $i ; gfortran gtk.o gtk-sup.o $i  `pkg-config --cflags --libs gtk+-3.0` -o $i.out ; done
-gfortran gtk.o ../examples/gtkbuilder2.f90 -o ../examples/gtkbuilder2.f90.out `pkg-config --cflags --libs gtk+-3.0` `pkg-config --cflags --libs gmodule-2.0`
+for i in ../examples/*.f90 ; do 
+  echo $i
+  gfortran gtk.o gtk-sup.o gtk-hl.o $i $gtkversion -o $i.out
+done
+gfortran gtk.o ../examples/gtkbuilder2.f90 -o ../examples/gtkbuilder2.f90.out $gtkversion `pkg-config --cflags --libs gmodule-2.0`
 
+echo "running the examples..."
 cd ../examples/
-
-for i in `ls -tr *.out` ; do ./$i ; done
+for i in *.out ; do ./$i ; done
