247f454940d9f3a30af0c07c06eb774d4dfab1b6
Vincent Magnin <vincent.magnin@libertysurf.fr>
2018-01-15 14:50:59 +0100

    Patch for gdk_pixbuf win32 functions (Ubuntu 17.10)

--- src/cfwrapper.py
+++ src/cfwrapper.py
@@ -25,7 +25,7 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 # Contributed by Vincent Magnin, 01.28.2011
-# Last modification: 10-11-2016 (Python 3.5.2, Linux Ubuntu 16.10)
+# Last modification: 01-15-2018 (Python 3.6.3, Linux Ubuntu 17.10)
 # pylint3 score: 8.69/10
 
 """ Generates the *-auto.f90 files from the C header files of GLlib and GTK+.
@@ -525,14 +525,36 @@ def write_fortran_interface(prototype, f_procedure, f_name, args_list, f_use, de
     interface3 += 0*TAB + f_the_end + "\n\n"
     interface = interface1+interface2+interface3
 
-    # For Win32 _utf8 functions, the normal form and the Windows form are
-    # dispatched in two platform dependent files, with the same module name:
-    if re.search(r"(?m)^#define\s+"+f_name+r"\s+"+f_name+r"_utf8\s*$",
-                 whole_file_original):
+    # For Win32 _utf8 functions, the normal form and the Windows form must be
+    # dispatched in two platform dependent files, with the same module name.
+    # Some _utf8 functions are defined by a #define, and others declared.
+    if ("gdk_pixbuf_new_from_file" in f_name) or ("gdk_pixbuf_savev" in f_name):
+        # gdk_pixbuf_new_from_file_utf8
+        # gdk_pixbuf_new_from_file_at_size_utf8
+        # gdk_pixbuf_new_from_file_at_scale_utf8
+        # gdk_pixbuf_savev
+        # Historically these functions were defined by #define for win32,
+        # so we maintain them in the gtk_os_dependent modules because
+        # the gdk-pixbuf-hl.f90 uses them.
+        # PROBABLY OTHER FUNCTIONS ARE IN THE SAME CASE.
+        # IN THE FUTURE, gdk-pixbuf-hl.f90 COULD BE INSTEAD MODIFIED.
+        if "_utf8" not in f_name:
+            print(f_name)
+            unix_only_file.write(interface)
+            index.append(["gtk_os_dependent", f_name,
+                        "unixonly-auto.f90/mswindowsonly-auto.f90",
+                        directory[0]+"/"+c_file_name, prototype, first_line])
+            first_line = 0*TAB + f_procedure + f_name + "(" + args_list + ") bind(c, name='"+f_name+"_utf8')"
+            interface2_utf8 = multiline(first_line, 80) + "\n"
+            mswindows_only_file.write(interface1+interface2_utf8+interface3)
+            nb_generated_interfaces += 2
+            nb_win32_utf8 += 1
+    elif re.search(r"(?m)^#define\s+"+f_name+r"\s+"+f_name+r"_utf8\s*$",
+                whole_file_original):
         unix_only_file.write(interface)
         index.append(["gtk_os_dependent", f_name,
-                      "unixonly-auto.f90/mswindowsonly-auto.f90",
-                      directory[0]+"/"+c_file_name, prototype, first_line])
+                    "unixonly-auto.f90/mswindowsonly-auto.f90",
+                    directory[0]+"/"+c_file_name, prototype, first_line])
         first_line = 0*TAB + f_procedure + f_name + "(" + args_list + ") bind(c, name='"+f_name+"_utf8')"
         interface2_utf8 = multiline(first_line, 80) + "\n"
         mswindows_only_file.write(interface1+interface2_utf8+interface3)
@@ -541,7 +563,7 @@ def write_fortran_interface(prototype, f_procedure, f_name, args_list, f_use, de
     else: # Non platform specific functions
         f_file.write(interface)
         index.append([module_name, f_name, f_file_name,
-                      directory[0]+"/"+c_file_name, prototype, first_line])
+                    directory[0]+"/"+c_file_name, prototype, first_line])
         nb_generated_interfaces += 1
 
 
