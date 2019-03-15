cec17cc3523053a04d9d2207dacc275ae1775d9c
vmagnin <vincent.magnin@libertysurf.fr>
2019-03-15 17:46:03 +0100

    show_versions.sh: improved layout, test for the OS

--- src/show_versions.sh
+++ src/show_versions.sh
@@ -2,28 +2,57 @@
 # Show libraries and tools versions used in gtk-fortran
 # Parameters: none
 # Contributed by Vincent MAGNIN, 2019-03-13
-# Updated 2019-03-13
+# Updated 2019-03-15
+
+echo "======="
+echo "SYSTEM:"
+echo "======="
 
 # Distribution (Linux only):
 lsb_release -sd
-
 # Kernel, release, processor, OS:
 uname -srpo
 
-# Libraries:
-dpkg-query --show libgtk2.0-dev
-dpkg-query --show libgtk-3-dev
-dpkg-query --show libplplot-dev
+echo "=========="
+echo "LIBRARIES:"
+echo "=========="
 
-# Languages:
+# Default package names:
+LIB_GTK2="libgtk2.0-dev"
+LIB_GTK3="libgtk-3-dev"
+LIB_GLIB="libglib2.0-dev"
+LIB_PLPLOT="libplplot-dev"
+
+if [ $(lsb_release -sd | grep -c Fedora) != 0 ]; then
+#     LIB_GTK2="libgtk2.0-dev"
+#     LIB_GTK3="libgtk-3-dev"
+#     LIB_GLIB="libglib2.0-dev"
+#     LIB_PLPLOT="libplplot-dev"
+    echo
+fi
+
+dpkg-query --show $LIB_GTK2
+dpkg-query --show $LIB_GTK3
+dpkg-query --show $LIB_GLIB
+dpkg-query --show $LIB_PLPLOT
+
+echo "=========="
+echo "LANGUAGES:"
+echo "=========="
+
 gfortran --version | head -n 1
 python3 --version
 perl --version | head -n 2 | tail -n 1
+bash --version | head -n 1
 
-# Build tools:
+echo "============"
+echo "BUILD TOOLS:"
+echo "============"
+
 cmake --version | head -n 1
 echo "pkg-config $(pkg-config --version)"
-
-# Other tools:
 git --version
-bash --version | head -n 1
+
+echo "============"
+echo "OTHER TOOLS:"
+echo "============"
