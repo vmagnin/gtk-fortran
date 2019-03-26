d36a9f5e053d773d5a3f87d911ce926ec7f5c958
vmagnin <vincent.magnin@libertysurf.fr>
2019-03-26 12:01:56 +0100

    src/build.sh: improvements

--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 # Script to build gtk-fortran using CMake
-# Vincent MAGNIN, 06-19-2016
+# Vincent MAGNIN, 2016-06-19, modified 2019-03-26
 
 echo "Building gtk-fortran..."
+
 cd ..
+
+# If the build directory already exists, this command will print an error but
+# the script execution will continue:
 mkdir build
 cd build
-cmake ..
-make -i
+
+make clean
+cmake -D CMAKE_BUILD_TYPE=debug ..
+
+# Parallel building:
+make -j
+
 echo
-echo "TO INSTALL gtk-fortran LIBRARIES ON YOUR SYSTEM, TYPE : cd ../build/ ; sudo make install"
+echo "TO INSTALL gtk-fortran LIBRARIES ON YOUR SYSTEM, TYPE :"
+echo "cd ../build/ ; sudo make install"
