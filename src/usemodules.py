ee8d9717d23c1b15ea6f566157ff17a0dca35b4a
vmagnin <vincent.magnin@libertysurf.fr>
2019-04-02 11:36:25 +0200

    usemodules.py: multiline() tries to cut before a space

--- src/usemodules.py
+++ src/usemodules.py
@@ -25,7 +25,7 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 # Contributed by Vincent Magnin, 04.04.2011
-# Last modification: vmagnin 2019-03-21
+# Last modification: vmagnin 2019-04-02
 
 import os
 import csv
@@ -34,14 +34,32 @@ import time
 import re        # Regular expression library
 import argparse  # To parse command line
 
-# TODO: cut lines outstide functions names.
-def multiline(ch, maxlength):
-    """Split a long line in a multiline, following Fortran syntax."""
+
+def multiline(line, max_length):
+    """Split a long line in a multiline, following Fortran syntax, and trying to
+       cut it with elegance.
+    """
+    max_offset = max_length-1
     result = ""
-    while len(ch) > maxlength-1:
-        result += ch[0:maxlength-1] + "&\n"
-        ch = "&"+ ch[maxlength-1:]
-    result += ch
+
+    while len(line) > max_offset:
+        # Remember that character max_length is excluded in such slice:
+        string = line[0:max_length]
+
+        cut = max_offset
+        # We try to cut before a space, if reasonably possible:
+        if string[cut] != " ":
+            # Search the last space after the middle of the string:
+            last_space = string.rfind(" ", max_length//2)
+            if last_space != -1:
+                cut = last_space
+
+        result += line[0:cut] + "&\n"
+        line = "&"+ line[cut:]
+
+    # Add last line without trailing spaces:
+    result += line.rstrip()
+
     return result
 
 
