b13932bf76289c79c25bef09c76baefc44238981
vmagnin <vincent.magnin@libertysurf.fr>
2019-04-02 09:52:46 +0200

    analyze.py: code cleaning

--- src/cfwrapper/analyze.py
+++ src/cfwrapper/analyze.py
@@ -25,16 +25,16 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 # Contributed by Vincent Magnin, 01.28.2011
-# Last modification: 2019-04-01
+# Last modification: 2019-04-02
 
-""" This module contains functions to analyze C prototypes and generate Fortran interfaces.
+""" This module contains functions to analyze C prototypes 
+    and generate Fortran interfaces.
 """
 
 import re           # Regular expression library
 
 # Project modules:
-from globals_const import *
-from errors import Errors
+from globals_const import TAB
 from fortran import iso_c_binding
 from tools import multiline
 
@@ -58,7 +58,11 @@ RGX_UNDERSCORE = re.compile(r"^_\w+$")
 
 # TODO: these procedures need refactoring !
 
-def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindows_only_file, f_file, preprocessed_list, whole_file_original, c_dir, c_file_name, gtk_enums, gtk_funptr, TYPES_DICT, TYPES2_DICT, my_stats, my_errors, ARGS):
+def analyze_prototypes(index, module_name, f_file_name, unix_only_file,
+                       mswindows_only_file, f_file, preprocessed_list,
+                       whole_file_original, c_dir, c_file_name, gtk_enums,
+                       gtk_funptr, TYPES_DICT, TYPES2_DICT, my_stats, my_errors,
+                       ARGS):
     """Each prototype of the preprocessed list is now analyzed.
     """
 
@@ -69,7 +73,7 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
         if "..." in proto:
             my_stats.inc_nb_variadic()
             my_errors.new_error(c_dir, c_file_name,
-                        "Variadic function", proto, False)
+                                "Variadic function", proto, False)
             continue    # Next prototype
 
         type_returned = RGX_RETURNED_TYPE.search(proto)
@@ -77,11 +81,11 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
             function_type = type_returned.group(1)
         except AttributeError:
             my_errors.new_error(c_dir, c_file_name,
-                        "Returned type not found", proto, False)
+                                "Returned type not found", proto, False)
             continue    # Next prototype
         if function_type == " ":
             my_errors.new_error(c_dir, c_file_name,
-                        "Returned type not found", proto, False)
+                                "Returned type not found", proto, False)
             continue    # Next prototype
 
         # Will it be a Fortran function or a subroutine ?
@@ -95,12 +99,13 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
             f_procedure = "function "
             f_the_end = "end function"
             isfunction = True
-            returned_type, iso_c = iso_c_binding(function_type, True, gtk_enums, gtk_funptr, TYPES_DICT, TYPES2_DICT)
+            returned_type, iso_c = iso_c_binding(function_type, True, gtk_enums,
+                                                 gtk_funptr, TYPES_DICT, TYPES2_DICT)
             f_use = iso_c
             if "?" in returned_type:    # Function type not found
                 error_flag = True
                 my_errors.new_error(c_dir, c_file_name, "Unknown type:  "
-                            + function_type, proto, True)
+                                    + function_type, proto, True)
                 continue
 
         # f_name will contain the name of the function in gtk-fortran:
@@ -109,7 +114,7 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
             f_name = function_name.group(1)
         except AttributeError:
             my_errors.new_error(c_dir, c_file_name,
-                        "Function name not found", proto, False)
+                                "Function name not found", proto, False)
             continue    # Next prototype
 
         # gtk_init() is already defined in gtk.f90. Other functions
@@ -120,12 +125,13 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
         # Functions beginning by an underscore will be excluded:
         if RGX_UNDERSCORE.match(f_name) is not None:
             my_errors.new_error(c_dir, c_file_name,
-                        "Function name beginning by underscore", proto, False)
+                                "Function name beginning by underscore", proto, False)
             continue    # Next prototype
 
         # What is the status of that function ? (Is the C prototype preceded
         # on the previous line by a DEPRECATED or AVAILABLE statement ?)
-        status = re.search(r"(?m)^(.*?(DEPRECATED|AVAILABLE).*?)\n.*?"+f_name+r"\W", whole_file_original)
+        status = re.search(r"(?m)^(.*?(DEPRECATED|AVAILABLE).*?)\n.*?" + f_name
+                           + r"\W", whole_file_original)
         if status:
             function_status = status.group(1)
             if "DEPRECATED" in function_status:
@@ -144,7 +150,7 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
             args = RGX_ARGS.findall(arguments.group(1))
         except AttributeError:
             my_errors.new_error(c_dir, c_file_name,
-                        "Problem determining the arguments", proto, False)
+                                "Problem determining the arguments", proto, False)
             continue    # Next prototype
 
         # Each argument of the function is analyzed:
@@ -160,11 +166,12 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
                 var_type = RGX_VAR_TYPE.search(arg).group(1)
             except AttributeError:
                 my_errors.new_error(c_dir, c_file_name,
-                            "Variable type not found", proto, True)
+                                    "Variable type not found", proto, True)
                 continue    # Next argument
 
             # Corresponding Fortran type of the argument:
-            f_type, iso_c = iso_c_binding(arg, False, gtk_enums, gtk_funptr, TYPES_DICT, TYPES2_DICT)
+            f_type, iso_c = iso_c_binding(arg, False, gtk_enums, gtk_funptr,
+                                          TYPES_DICT, TYPES2_DICT)
             if iso_c not in my_stats.used_types:
                 my_stats.append_type(iso_c)
 
@@ -180,14 +187,14 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
             elif "?" in f_type:     # Type not found
                 error_flag = True
                 my_errors.new_error(c_dir, c_file_name, "Unknown type:  " + arg,
-                            proto, True)
+                                    proto, True)
 
             # Search the variable name:
             try:
                 var_name = RGX_VAR_NAME.search(arg).group(1)
             except AttributeError:
                 my_errors.new_error(c_dir, c_file_name,
-                            "Variable name not found", proto, False)
+                                    "Variable name not found", proto, False)
                 continue    # Next argument
 
             # Unknown dimension arrays are passed by address, others by value:
@@ -206,10 +213,19 @@ def analyze_prototypes(index, module_name, f_file_name, unix_only_file, mswindow
 
         # Write the Fortran interface in the .f90 file:
         if not error_flag:
-            write_fortran_interface(index, module_name, f_file_name, unix_only_file, mswindows_only_file, f_file, whole_file_original, c_dir, c_file_name, function_status, proto, f_procedure, f_name, args_list, f_use, declarations, isfunction, returned_type, f_the_end, my_stats)
+            write_fortran_interface(index, module_name, f_file_name,
+                                    unix_only_file, mswindows_only_file, f_file,
+                                    whole_file_original, c_dir, c_file_name,
+                                    function_status, proto, f_procedure, f_name,
+                                    args_list, f_use, declarations, isfunction,
+                                    returned_type, f_the_end, my_stats)
 
 
-def write_fortran_interface(index, module_name, f_file_name, unix_only_file, mswindows_only_file, f_file, whole_file_original, c_dir, c_file_name, function_status, prototype, f_procedure, f_name, args_list, f_use, declarations, isfunction, returned_type, f_the_end, my_stats):
+def write_fortran_interface(index, module_name, f_file_name, unix_only_file,
+                            mswindows_only_file, f_file, whole_file_original,
+                            c_dir, c_file_name, function_status, prototype,
+                            f_procedure, f_name, args_list, f_use, declarations,
+                            isfunction, returned_type, f_the_end, my_stats):
     """Write the Fortran interface of a function in the *-auto.f90 file
     """
     interface1 = 0*TAB + "! " + function_status + "\n"
@@ -226,7 +242,7 @@ def write_fortran_interface(index, module_name, f_file_name, unix_only_file, msw
     # Names for the gtk-fortran-index.csv file:
     my_module_name = module_name
     my_f_file_name = f_file_name
-    my_first_line  = first_line
+    my_first_line = first_line
 
     # For Win32 _utf8 functions, the normal form and the Windows form must be
     # dispatched in two platform dependent files, with the same module name.
@@ -241,7 +257,10 @@ def write_fortran_interface(index, module_name, f_file_name, unix_only_file, msw
         # PROBABLY OTHER FUNCTIONS ARE IN THE SAME CASE.
         # IN THE FUTURE, gdk-pixbuf-hl.f90 COULD BE INSTEAD MODIFIED.
 
-    if ((("gdk_pixbuf_new_from_file" in f_name) or ("gdk_pixbuf_savev" in f_name)) and ("_utf8" not in f_name)) or re.search(r"(?m)^#define\s+"+f_name+r"\s+"+f_name+r"_utf8\s*$", whole_file_original):
+    if ((("gdk_pixbuf_new_from_file" in f_name) or ("gdk_pixbuf_savev" in f_name))
+            and ("_utf8" not in f_name)) or re.search(r"(?m)^#define\s+" + f_name
+                                                      + r"\s+"+f_name+r"_utf8\s*$",
+                                                      whole_file_original):
         # The re.search() test is for GTK 2.
         # In recent versions of GTK 3, there is no more functions defined
         # like this (verified in GTK 3.24.4).
