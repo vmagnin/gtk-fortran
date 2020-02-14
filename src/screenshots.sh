59e94a71ec59b29db82cc44078758f31f6750d89
vmagnin <vincent.magnin@libertysurf.fr>
2020-02-14 14:44:19 +0100

    src/screenshots.sh: improved code

--- src/screenshots.sh
+++ src/screenshots.sh
@@ -6,33 +6,36 @@
 # Contributed by Vincent MAGNIN 2011-04-27
 # Updated 2020-02-12
 
+# For a safer script:
+set -eu
+
 if [ $# -eq 0 ]; then
   seconds=3
   suffix="-kubuntu_"$(lsb_release -rs)
 else
-  seconds=$2
-  suffix=$1
+  seconds=${2}
+  suffix=${1}
 fi
 
-echo "Suffix: "$suffix
-echo "Sleep time: "$seconds
+echo "Suffix: "${suffix}
+echo "Sleep time: "${seconds}
 echo "Taking screenshots..."
 
 # Scanning all built examples:
 for directory in ../build/examples/ ../build/plplot ../build/sketcher ; do
-    cd $directory
+    cd ${directory}
     pwd
     for file in * ; do
         # Is it an executable file ?
-        if [ -x "$file" ] && [ ! -d "$file" ]; then
-            echo $file
+        if [ -x "${file}" ] && [ ! -d "${file}" ]; then
+            echo ${file}
             #Launch the program:
-            ./$file &
-            sleep $seconds
+            ./${file} &
+            sleep ${seconds}
             #remove the double extension (seven characters after a point):
-            picfile=`echo $file|sed 's/\..\{7\}$//'`
+            picfile=$(echo ${file}|sed 's/\..\{7\}$//')
             #Take and save a screenshot of the active window with border:
-            scrot -ub ../../screenshots/$picfile$suffix.png
+            scrot -ub ../../screenshots/${picfile}${suffix}.png
             #Kill the program before launching next one:
             kill $!
         fi
