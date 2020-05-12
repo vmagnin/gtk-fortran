b5ffb1053929576c25185a468597d878f40f3e89
vmagnin <vincent.magnin@libertysurf.fr>
2020-05-12 14:34:58 +0200

    examples/gtkhello.f90: new version based on GtkApplication and GApplication

--- examples/gtkhello.f90
+++ examples/gtkhello.f90
@@ -20,50 +20,104 @@
 ! this program; see the files COPYING3 and COPYING.RUNTIME respectively.
 ! If not, see <http://www.gnu.org/licenses/>.
 !-------------------------------------------------------------------------------
-! Vincent MAGNIN et al. (derived from gtkhello2.f90), 2019-02-15
-! Last modified: 2020-02-12
+! Vincent MAGNIN, 2020-05-12, based on gtkzero_gapp.f90
+! Last modified: 2020-05-12
+! A GTK application with two buttons.
+! https://developer.gnome.org/gtk4/unstable/gtk-getting-started.html
+! https://developer.gnome.org/gio/stable/GApplication.html
 !-------------------------------------------------------------------------------
 
 !*************************************
 ! User defined event handlers go here
 !*************************************
-! Note that events are a special type of signals, coming from the X Window system.
-! Callback functions must have an event argument.
+! Note that events are a special type of signals, coming from 
+! the X Window system. Callback functions must have an event argument.
 module handlers
 
-  use iso_c_binding, only: c_ptr, c_int
-  use gtk, only: gtk_main_quit, FALSE
+  use iso_c_binding, only: c_ptr, c_int, c_funloc
+  use gtk, only: FALSE, c_null_char, gtk_window_set_default_size,  &
+               & gtk_window_set_title, gtk_widget_destroy, &
+               & g_signal_connect, g_signal_connect_swapped, &
+               & gtk_widget_show, gtk_application_window_new, &
+               & gtk_box_new, gtk_container_add, &
+               & GTK_ORIENTATION_VERTICAL, GTK_ORIENTATION_HORIZONTAL, &
+               & gtk_button_new_with_label, gtk_button_new_with_mnemonic, &
+               & gtk_widget_set_margin_start, gtk_widget_set_margin_end, &
+               & gtk_widget_set_margin_top, gtk_widget_set_margin_bottom
   implicit none
 
 contains
-  ! Signal emitted if a user requests that a toplevel window is closed:
-  function delete_event(widget, event, gdata) result(ret)  bind(c)
-    integer(c_int)     :: ret
-    type(c_ptr), value :: widget, event, gdata
+  ! Callback function for the signal "activate" emitted by g_application_run().
+  ! We use a subroutine because it should return void.
+  ! The GUI is defined here.
+  subroutine activate(app, gdata) bind(c)
+    type(c_ptr), value, intent(in)  :: app, gdata
+    ! Pointers toward our GTK widgets:
+    type(c_ptr) :: window
+    type(c_ptr) :: box
+    type(c_ptr) :: button1, button2
 
-    print *, "My delete_event"
-    ret = FALSE
-  end function delete_event
+    ! Create the window:
+    window = gtk_application_window_new(app)
+    ! Not compulsory, but can be used if you want a larger window:
+    !call gtk_window_set_default_size(window, 300, 200)
+    ! Don't forget that C strings must end with a null char:
+    call gtk_window_set_title(window, "Hello GLib & GTK world!"//c_null_char)
 
-  ! "destroy" is a GtkObject signal emitted when an object is destroyed:
-  subroutine destroy(widget, gdata) bind(c)
-    type(c_ptr), value :: widget, gdata
+    !******************************************************************
+    ! Adding widgets in the window:
+    !******************************************************************
+    ! You need a box where to arrange your buttons, separated by 10 pixels:
+    box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10_c_int);
+    ! Perhaps you prefer a vertical organization:
+    !box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10_c_int);
+    ! Set the border width (10 pixels) around the box:
+    call gtk_widget_set_margin_start (box, 10_c_int)
+    call gtk_widget_set_margin_end (box, 10_c_int)
+    call gtk_widget_set_margin_top (box, 10_c_int)
+    call gtk_widget_set_margin_bottom (box, 10_c_int)
+    ! You need a container where to put the box, it will manage layout:
+    call gtk_container_add(window, box)
 
-    print *, "My destroy"
-    call gtk_main_quit()
-  end subroutine destroy
+    ! It's easy to create a button:
+    button1 = gtk_button_new_with_label("I say hello"//c_null_char)
+    ! Let's pack the button in the box:
+    call gtk_container_add(box, button1)
+    ! You can associate one or several callback functions with the button,
+    ! "clicked" is a GtkButton signal emitted when you click on it:
+    call g_signal_connect(button1, "clicked"//c_null_char, &
+                        & c_funloc(button1clicked))
+    call g_signal_connect(button1, "clicked"//c_null_char, c_funloc(hello))
 
+    ! For that second button, there is an ALT+g keyboard shortcut:
+    button2 = gtk_button_new_with_mnemonic("I don't know why you say _goodbye"&
+                                          &//c_null_char)
+    ! Let's pack the second button in the box (from left to right):
+    call gtk_container_add(box, button2)
+    ! Let's associate one callback function when that button is clicked.
+    ! Here the gtk_widget_destroy() function will be applied
+    ! to window instead of button2:
+    call g_signal_connect_swapped(button2, "clicked"//c_null_char, &
+                                & c_funloc(gtk_widget_destroy), window)
+    !******************************************************************
+
+    ! If you don't show it, nothing will appear on screen...
+    call gtk_widget_show(window)
+
+  end subroutine activate
+
+  ! The two callback functions for the button1:
   function hello(widget, gdata) result(ret)  bind(c)
-    integer(c_int)     :: ret
-    type(c_ptr), value :: widget, gdata
+    integer(c_int)                  :: ret
+    type(c_ptr), value, intent(in)  :: widget, gdata
 
     print *, "So I say Hello GTK World!"
     ret = FALSE
   end function hello
 
   function button1clicked(widget, gdata) result(ret)  bind(c)
-    integer(c_int)     :: ret
-    type(c_ptr), value :: widget, gdata
+    integer(c_int)                  :: ret
+    type(c_ptr), value, intent(in)  :: widget, gdata
 
     print *, "Button 1 clicked!"
     ret = FALSE
@@ -71,91 +125,44 @@ contains
 
 end module handlers
 
-!*********************************************************************************************
-! In the main program, we initialize GTK, we design the UI, and finally call the GTK main loop
-! For more information on GTK functions: https://developer.gnome.org/gtk3/stable/
-!*********************************************************************************************
+!*******************************************************************************
+! In the main program, we declare the GTK application, connect it to its 
+! "activate" function where we will create the GUI, 
+! and finally call the GLib main loop.
+!*******************************************************************************
 program gtkhello
 
-  use iso_c_binding, only: c_ptr, c_funloc, c_int
+  use iso_c_binding, only: c_ptr, c_funloc
   ! We will use those GTK functions and values. The "only" statement can improve
   ! significantly the compilation time:
-  use gtk, only: gtk_init, gtk_window_new, gtk_window_set_default_size, gtk_window_set_title, &
-               & g_signal_connect, gtk_box_new, gtk_container_add, &
-               & gtk_button_new_with_label, gtk_button_new_with_mnemonic, &
-               & gtk_widget_show, gtk_widget_show_all, gtk_main, &
-               & GTK_WINDOW_TOPLEVEL, c_null_char, GTK_ORIENTATION_HORIZONTAL,&
-               & gtk_widget_set_margin_start, gtk_widget_set_margin_end, &
-               & gtk_widget_set_margin_top, gtk_widget_set_margin_bottom
+  use gtk, only: c_null_char, c_null_ptr, gtk_application_new, &
+               & G_APPLICATION_FLAGS_NONE
+  use g, only: g_application_run, g_object_unref
   use handlers
 
   implicit none
-  ! Pointers toward our GTK widgets:
-  type(c_ptr) :: window
-  type(c_ptr) :: box
-  type(c_ptr) :: button1, button2
+  integer(c_int)     :: status
+  type(c_ptr)        :: app
 
-  ! First, the GTK library must be initialized:
-  call gtk_init()
-  ! If you want to pass to GTK some command line arguments, see gtk.f90.
+  ! First, let's create a GTK application (it will initialize GTK).
+  ! The application ID must contain at least one point:
+  ! https://developer.gnome.org/gio/stable/GApplication.html#g-application-id-is-valid
+  app = gtk_application_new("gtk-fortran.examples.gtkhello"//c_null_char, &
+                            & G_APPLICATION_FLAGS_NONE)
+  ! The activate signal will be sent by g_application_run(). 
+  ! The c_funloc() function returns the C address of the callback function.
+  ! The c_null_ptr means no data is transfered to the callback function.
+  call g_signal_connect(app, "activate"//c_null_char, c_funloc(activate), &
+                      & c_null_ptr)
+  ! Now, the whole application will be managed by GLib (=> main loop).
+  ! Note that commandline arguments argc, argv are not passed.
+  ! https://developer.gnome.org/gio/stable/GApplication.html#g-application-run
+  status = g_application_run(app, 0_c_int, c_null_ptr)
 
-  ! Create the window and set up some signals for it.
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL)
-  ! Not compulsory, but can be used if you want a larger window:
-  !call gtk_window_set_default_size(window, 400, 50)
+  print *, "You have exited the GLib main loop, bye, bye..."
 
-  ! Don't forget that C strings must end with a null char:
-  call gtk_window_set_title(window, "Hello GTK world!"//c_null_char)
-
-  ! Let's define two events for that window. The c_funloc() function returns
-  ! the C address of the callback function.
-  ! When you click on the cross to exit the program or close it from the window menu:
-  call g_signal_connect(window, "delete-event"//c_null_char, c_funloc(delete_event))
-  ! Before quitting the GTK main loop:
-  call g_signal_connect(window, "destroy"//c_null_char, c_funloc(destroy))
-
-  !******************************************************************
-  ! Widgets in the window:
-  !******************************************************************
-  ! You need a box where to arrange your buttons, separated by 10 pixels:
-  box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10_c_int);
-  ! Perhaps you prefer a vertical organization:
-  !box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10_c_int);
-  ! This example can not be built with GTK 2 because gtk_box_nex() appeared
-  ! in GTK3.
-  ! Set the border width (10 pixels) around the box:
-  call gtk_widget_set_margin_start (box, 10_c_int)
-  call gtk_widget_set_margin_end (box, 10_c_int)
-  call gtk_widget_set_margin_top (box, 10_c_int)
-  call gtk_widget_set_margin_bottom (box, 10_c_int)
-  ! You need a container where to put the box, it will manage layout:
-  call gtk_container_add(window, box)
-  
-  ! It's easy to create a button:
-  button1 = gtk_button_new_with_label("I say hello"//c_null_char)
-  ! Let's pack the button in the box.
-  call gtk_container_add(box, button1)
-  ! Let's associate two callback functions when that button is clicked:
-  call g_signal_connect(button1, "clicked"//c_null_char, c_funloc(button1clicked))
-  ! "clicked" is a GtkButton signal emitted when you click on a button:
-  call g_signal_connect(button1, "clicked"//c_null_char, c_funloc(hello))
-
-  ! For that button, there is an ALT+g keyboard shortcut:
-  button2 = gtk_button_new_with_mnemonic("I don't know why you say _goodbye"//c_null_char)
-  ! Let's pack the second button in the box (packing occurs from left to right):
-  call gtk_container_add(box, button2)
-  ! Let's associate one callback function when that button is clicked:
-  call g_signal_connect(button2, "clicked"//c_null_char, c_funloc(destroy))
-  !******************************************************************
-
-  ! If you don't show it, nothing will appear on screen...
-  call gtk_widget_show_all(window)
-  ! You could also show the widgets one by one by calling gtk_widget_show()
-  ! for button1, button2, box and window.
-
-  ! Now, the events will be handled by the main GTK loop:
-  call gtk_main()
-
-  print *, "You have exited the GTK main loop, bye, bye..."
+  ! Memory is freed:
+  call g_object_unref(app)
 
 end program gtkhello
+
