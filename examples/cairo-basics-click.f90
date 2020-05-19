d210a2b3f78b1db6562945b0b08e39f9cf35342d
vmagnin <vincent.magnin@libertysurf.fr>
2020-05-19 15:58:05 +0200

    examples/cairo-basics-click.f90: new GTK4 version

--- examples/cairo-basics-click.f90
+++ examples/cairo-basics-click.f90
@@ -1,195 +1,188 @@
 ! Copyright (C) 2011
 ! Free Software Foundation, Inc.
-
+!
 ! This file is part of the gtk-fortran gtk+ Fortran Interface library.
-
+!
 ! This is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
 ! the Free Software Foundation; either version 3, or (at your option)
 ! any later version.
-
+!
 ! This software is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-
+!
 ! Under Section 7 of GPL version 3, you are granted additional
 ! permissions described in the GCC Runtime Library Exception, version
 ! 3.1, as published by the Free Software Foundation.
-
+!
 ! You should have received a copy of the GNU General Public License along with
 ! this program; see the files COPYING3 and COPYING.RUNTIME respectively.
 ! If not, see <http://www.gnu.org/licenses/>.
 !
-! Contributed by Jerry DeLisle and Vincent Magnin
-! Event handling: James Tappin
-! Last modification: vmagnin 2020-02-11
+! GTK 4 version contributed by Vincent Magnin
+! Last modification: vmagnin 2020-05-18
 
 module handlers
-  use iso_c_binding
+  use iso_c_binding, only: c_int, c_ptr, c_null_ptr, c_null_funptr, &
+                         & c_funloc, c_null_char, c_double, c_bool
 
-  use cairo, only: cairo_arc, cairo_curve_to, cairo_destroy, &
-       & cairo_get_target, cairo_line_to, cairo_move_to, cairo_new_sub_path, &
-       & cairo_select_font_face, cairo_set_font_size, cairo_set_line_width, &
-       & cairo_set_source_rgb, cairo_show_text, cairo_stroke, &
-       & cairo_surface_write_to_png
+  use gtk, only: gtk_application_window_new, gtk_drawing_area_new, &
+  & gtk_drawing_area_set_content_width, gtk_drawing_area_set_content_height, &
+  & gtk_drawing_area_set_draw_func, gtk_container_add, gtk_widget_show, &
+  & gtk_window_set_default_size, gtk_window_set_title, FALSE, &
+  & gtk_gesture_click_new, gtk_widget_add_controller, &
+  & gtk_event_controller_get_widget, g_signal_connect, &
+  & gtk_widget_get_width, gtk_widget_get_height, &
+  & gtk_gesture_single_get_current_button, gtk_gesture_single_set_button, &
+  & gtk_event_controller_scroll_new, GTK_EVENT_CONTROLLER_SCROLL_VERTICAL, &
+  & gtk_widget_queue_draw
 
-  use gtk, only: gtk_container_add, gtk_drawing_area_new, gtk_event_box_new, &
-       & gtk_events_pending, gtk_main_iteration_do, gtk_widget_add_events, &
-       & gtk_widget_show_all, gtk_window_new, &
-       & gtk_window_set_default_size, gtk_window_set_title, gtk_init, &
-       & g_signal_connect, TRUE, FALSE, GDK_BUTTON_PRESS_MASK, &
-       & GDK_SCROLL_MASK, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL, &
-       & GTK_WINDOW_TOPLEVEL, gtk_main, gtk_main_quit
-
-  use gdk_events
+  use cairo, only: cairo_arc, cairo_line_to, cairo_move_to, &
+  & cairo_set_line_width, cairo_set_source_rgb, cairo_stroke, &
+  & cairo_get_target, cairo_surface_write_to_png
 
   implicit none
-  integer(c_int) :: run_status = TRUE
-  integer(c_int) :: boolresult
-  logical :: boolevent
-  integer(kind=c_int) :: width, height
-
+  ! Circle radius:
+  real(8) :: radius = 100d0
 
 contains
-  ! User defined event handlers go here
-  function delete_event (widget, event, gdata) result(ret)  bind(c)
-    use iso_c_binding, only: c_ptr, c_int
-    integer(c_int)    :: ret
-    type(c_ptr), value :: widget, event, gdata
-    run_status = FALSE
-    ret = FALSE
-    call gtk_main_quit()
-  end function delete_event
+  ! The GUI is defined here:
+  subroutine activate(app, gdata) bind(c)
+    type(c_ptr), value, intent(in)  :: app, gdata
+    type(c_ptr)     :: window
+    type(c_ptr)     :: my_drawing_area, controller, controller2
+    integer(c_int)  :: width, height
+       
+    window = gtk_application_window_new(app)
+    width  = 700
+    height = 700
+    call gtk_window_set_default_size(window, width, height)
+    call gtk_window_set_title(window, "Click or scroll and I will tell you..."//c_null_char)
 
+    my_drawing_area = gtk_drawing_area_new()
+    call gtk_drawing_area_set_content_width(my_drawing_area, width)
+    call gtk_drawing_area_set_content_height(my_drawing_area, height)
+    call gtk_drawing_area_set_draw_func(my_drawing_area, &
+                   & c_funloc(my_draw_function), c_null_ptr, c_null_funptr)
 
-  subroutine pending_events ()
-    do while(IAND(gtk_events_pending(), run_status) /= FALSE)
-       boolresult = gtk_main_iteration_do(FALSE) ! False for non-blocking
+    ! We need a gesture controller to detect mouse clicks: 
+    ! https://developer.gnome.org/gtk4/stable/GtkGestureClick.html
+    ! https://developer.gnome.org/gtk4/stable/GtkWidget.html#gtk-widget-add-controller
+    controller = gtk_gesture_click_new()
+    ! 0 to listen to all buttons (button 1 by default):
+    call gtk_gesture_single_set_button (controller, 0_c_int)
+    call g_signal_connect(controller, "pressed"//c_null_char, &
+                                        & c_funloc(click_cb))
+    call gtk_widget_add_controller(my_drawing_area, controller)
+
+    ! And a controller for scrolling (modifies the circle radius):
+    ! https://developer.gnome.org/gtk4/stable/GtkEventControllerScroll.html
+    controller2 = gtk_event_controller_scroll_new (GTK_EVENT_CONTROLLER_SCROLL_VERTICAL)
+    call g_signal_connect(controller2, "scroll"//c_null_char, &
+                                        & c_funloc(scroll_cb))
+    call gtk_widget_add_controller(my_drawing_area, controller2)
+
+    call gtk_container_add(window, my_drawing_area)
+    call gtk_widget_show(window)
+  end subroutine activate
+
+  ! Click callback function ("pressed" signal):
+  subroutine click_cb(gesture, n_press, x, y, gdata) bind(c)
+    type(c_ptr), value, intent(in)    :: gesture, gdata
+    integer(c_int), value, intent(in) :: n_press
+    real(c_double), value, intent(in) :: x, y
+    type(c_ptr) :: widget
+    integer(c_int) :: width, height
+    real(8) :: d
+
+    print *, "Button ", gtk_gesture_single_get_current_button(gesture)
+
+    widget = gtk_event_controller_get_widget(gesture)
+    print *, n_press, " click(s) at ", int(x), int(y)
+
+    width  = gtk_widget_get_width(widget)
+    height = gtk_widget_get_height(widget)
+    d = sqrt((x - width/2d0)**2 + (y - height/2d0)**2)
+    print *, "Distance from the centre of the circle: ", d
+  end subroutine click_cb
+
+  ! Scroll callback function ("scroll" signal):
+  function scroll_cb(controller, x, y, gdata) result(ret) bind(c)
+    type(c_ptr), value, intent(in)    :: controller, gdata
+    real(c_double), value, intent(in) :: x, y
+    logical(c_bool) :: ret
+    type(c_ptr) :: widget
+
+    radius = radius + y
+    print *, "Scroll and new circle radius: ", x, y, radius
+    ! We need to redraw the area:
+    widget = gtk_event_controller_get_widget(controller)
+    call gtk_widget_queue_draw(widget);
+
+    ret = .true.
+  end function scroll_cb
+
+  ! "It is called whenever GTK needs to draw the contents of the drawing area
+  ! to the screen."
+  ! https://developer.gnome.org/gtk4/stable/GtkDrawingArea.html#gtk-drawing-area-set-draw-func
+  subroutine my_draw_function(widget, my_cairo_context, width, height, gdata) bind(c)
+    type(c_ptr), value, intent(in)    :: widget, my_cairo_context, gdata
+    integer(c_int), value, intent(in) :: width, height    
+    integer(c_int)                    :: cstatus
+    integer                           :: t
+    real(8), parameter                :: pi = 3.14159265358979323846d0
+print *, "draw"
+    ! https://cairographics.org/manual/
+    ! Note that Cairo numerical parameters are generally doubles.
+
+    ! Horizontal and vertical thin blue lines:
+    call cairo_set_source_rgb(my_cairo_context, 0d0, 0d0, 1d0)
+    call cairo_set_line_width(my_cairo_context, 1d0)
+ 
+    do t = 0, height, +100
+      ! https://cairographics.org/manual/cairo-Paths.html#cairo-move-to
+      call cairo_move_to(my_cairo_context, 0d0,       t*1d0)
+      call cairo_line_to(my_cairo_context, width*1d0, t*1d0)
+      call cairo_stroke(my_cairo_context) 
     end do
-  end subroutine pending_events
+ 
+    do t = 0, width, +100
+      call cairo_move_to(my_cairo_context, t*1d0, 0d0)
+      call cairo_line_to(my_cairo_context, t*1d0, height*1d0)
+      call cairo_stroke(my_cairo_context) 
+    end do
 
-  subroutine button_event(widget, event, gdata) bind(c)
-    type(c_ptr), value, intent(in) :: widget, event, gdata
-
-    type(gdkeventbutton), pointer :: bevent
-
-    print *, "Button press detected"
-    if (c_associated(event)) then
-       call c_f_pointer(event,bevent)
-       print *, "Clicked at:", int(bevent%x), int(bevent%y)
-       print *, "Type:", bevent%type
-       print *, "State, Button:", bevent%state, bevent%button
-       print *, "Root x,y:", int(bevent%x_root), int(bevent%y_root)
-    end if
-    print *
-  end subroutine button_event
-
-  subroutine scroll_event(widget, event, gdata) bind(c)
-    type(c_ptr), value, intent(in) :: widget, event, gdata
-
-    type(gdkeventscroll), pointer :: bevent
-
-    print *, "Wheel event detected"
-    if (c_associated(event)) then
-       call c_f_pointer(event,bevent)
-       print *, "Clicked at:", int(bevent%x), int(bevent%y)
-       print *, "State, direction:", bevent%state, bevent%direction
-       print *, "Root x,y:", int(bevent%x_root), int(bevent%y_root)
-    end if
-    print *
-  end subroutine scroll_event
-
-
-  function expose_event (widget, my_cairo_context, gdata) result(ret)  bind(c)
-    use iso_c_binding, only: c_int, c_ptr
-    implicit none
-    real(8), parameter :: pi = 3.14159265358979323846d0
-    integer(c_int)    :: ret
-    type(c_ptr), value, intent(in) :: widget, my_cairo_context, gdata
-    integer :: cstatus
-    integer :: t
-
-    ! Bezier curve:
-    call cairo_set_source_rgb(my_cairo_context, 0.9d0, 0.8d0, 0.8d0)
-    call cairo_set_line_width(my_cairo_context, 4d0)
-    call cairo_move_to(my_cairo_context, 0d0, 0d0)  
-    call cairo_curve_to(my_cairo_context, 600d0, 50d0, 115d0, 545d0, width*1d0, height*1d0)
+    ! A thick red circle at the centre:
+    call cairo_set_source_rgb(my_cairo_context, 1d0, 0d0, 0d0)
+    call cairo_set_line_width(my_cairo_context, 5d0)
+    call cairo_arc(my_cairo_context, width/2d0, height/2d0, radius, 0d0, 2*pi)
     call cairo_stroke(my_cairo_context) 
 
-    ! Lines:
-    call cairo_set_source_rgb(my_cairo_context, 0d0, 0.5d0, 0.5d0)
-    call cairo_set_line_width(my_cairo_context, 2d0)
-    do t = 0, int(height), +20
-       call cairo_move_to(my_cairo_context, 0d0, t*1d0)  
-       call cairo_line_to(my_cairo_context, t*1d0, height*1d0)
-       call cairo_stroke(my_cairo_context) 
-    end do
-
-    ! Text:
-    call cairo_set_source_rgb(my_cairo_context, 0d0, 0d0, 1d0)
-    call cairo_select_font_face(my_cairo_context, "Times"//c_null_char, CAIRO_FONT_SLANT_NORMAL, &
-         &  CAIRO_FONT_WEIGHT_NORMAL)
-    call cairo_set_font_size (my_cairo_context, 40d0)
-    call cairo_move_to(my_cairo_context, 100d0, 30d0)
-    call cairo_show_text (my_cairo_context, "gtk-fortran"//c_null_char)
-    call cairo_move_to(my_cairo_context, 100d0, 75d0)
-    call cairo_show_text (my_cairo_context, "Cairo & Fortran are good friends"//c_null_char)
-
-    ! Circles:
-    call cairo_new_sub_path(my_cairo_context)
-    do t = 1, 50
-       call cairo_set_source_rgb(my_cairo_context, t/50d0, 0d0, 0d0)
-       call cairo_set_line_width(my_cairo_context, 5d0*t/50d0)
-       call cairo_arc(my_cairo_context, 353d0+200d0*cos(t*2d0*pi/50), 350d0+200d0*sin(t*2d0*pi/50), 50d0, 0d0, 2*pi) 
-       call cairo_stroke(my_cairo_context) 
-    end do
-
-    ! Save:
-    cstatus = cairo_surface_write_to_png(cairo_get_target(my_cairo_context), "cairo.png"//c_null_char)
-
-    ret = FALSE
-  end function expose_event
+    ! Save the image as a PNG
+    ! https://cairographics.org/manual/cairo-PNG-Support.html#cairo-surface-write-to-png
+    cstatus = cairo_surface_write_to_png(cairo_get_target(my_cairo_context), &
+                                       & "cairo-basics-click.png"//c_null_char)
+  end subroutine my_draw_function
 end module handlers
 
-
+! We create a GtkApplication:
 program cairo_basics_click
-  use iso_c_binding, only: c_ptr, c_funloc
-  use handlers
+  use iso_c_binding, only: c_int, c_ptr, c_funloc, c_null_char, c_null_ptr
+  use gtk, only: gtk_application_new, g_signal_connect, G_APPLICATION_FLAGS_NONE
+  use g, only: g_application_run, g_object_unref
+  use handlers, only: activate
+
   implicit none
-  type(c_ptr) :: my_window
-  type(c_ptr) :: my_drawing_area
-  type(c_ptr) :: my_event_box
-  integer(kind=c_int), parameter :: ev_mask = ior(GDK_BUTTON_PRESS_MASK, &
-       & GDK_SCROLL_MASK)
-  call gtk_init ()
+  integer(c_int) :: exit_status
+  type(c_ptr)    :: app
 
-  ! Properties of the main window :
-  width = 700
-  height = 700
-  my_window = gtk_window_new (GTK_WINDOW_TOPLEVEL)
-  call gtk_window_set_default_size(my_window, width, height)
-  call gtk_window_set_title(my_window, "Cairo events demo"//c_null_char)
-  call g_signal_connect (my_window, "delete-event"//c_null_char, c_funloc(delete_event))
-
-  my_drawing_area = gtk_drawing_area_new()
-  my_event_box=gtk_event_box_new()
-  call gtk_container_add(my_event_box, my_drawing_area)
-  call gtk_widget_add_events(my_event_box, ev_mask)
-
-  ! In GTK 3 "expose-event" was replaced by "draw" event:
-  call g_signal_connect (my_drawing_area, "draw"//c_null_char, c_funloc(expose_event))
-  call g_signal_connect (my_event_box, "button_press_event"//c_null_char, &
-       & c_funloc(button_event))
-  call g_signal_connect (my_event_box, "scroll_event"//c_null_char, &
-       & c_funloc(scroll_event))
-  call gtk_container_add(my_window, my_event_box)
-
-  call gtk_widget_show_all (my_window)
-
-  ! The window stays opened after the computation
-  ! Main loop:
-  call gtk_main()
-  print *, "All done"
-
+  app = gtk_application_new("gtk-fortran.examples.cairo-basics-click"//c_null_char, &
+                            & G_APPLICATION_FLAGS_NONE)
+  call g_signal_connect(app, "activate"//c_null_char, c_funloc(activate), &
+                      & c_null_ptr)
+  exit_status = g_application_run(app, 0_c_int, c_null_ptr)
+  call g_object_unref(app)
 end program cairo_basics_click
+
