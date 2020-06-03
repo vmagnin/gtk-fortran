f419fa2bc54b65e426f9fbcf6e6f9958a41628d2
vmagnin <vincent.magnin@libertysurf.fr>
2020-06-03 11:44:55 +0200

    examples/hl_radio.f90: running with GTK 3.98.4, but with problems

--- examples/hl_radio.f90
+++ examples/hl_radio.f90
@@ -1,52 +1,54 @@
 ! Copyright (C) 2011
 ! Free Software Foundation, Inc.
-
+!
 ! This file is part of the gtk-fortran gtk+ Fortran Interface library.
-
+!
 ! This is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
 ! the Free Software Foundation; either version 3, or (at your option)
 ! any later version.
-
+!
 ! This software is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-
+!
 ! Under Section 7 of GPL version 3, you are granted additional
 ! permissions described in the GCC Runtime Library Exception, version
 ! 3.1, as published by the Free Software Foundation.
-
+!
 ! You should have received a copy of the GNU General Public License along with
 ! this program; see the files COPYING3 and COPYING.RUNTIME respectively.
 ! If not, see <http://www.gnu.org/licenses/>.
-!
+!------------------------------------------------------------------------------
 ! Contributed by James Tappin.
-! Last modification: vmagnin 02-20-2019
+! Last modification: vmagnin 2020-06-03 (GTK 4 version)
+!------------------------------------------------------------------------------
 
 module rb_handlers
-
-  use gtk_hl
-  use gtk, only: gtk_button_new, gtk_container_add, gtk_main, gtk_main_quit, gtk_&
-       &widget_destroy, gtk_radio_button_new, gtk_toggle_button_get_active, gtk_widget&
-       &_show, gtk_window_new, gtk_init
+!  use gtk_hl
+  use gtk_hl_container
+  use gtk_hl_button
+  use gtk, only: gtk_button_new, gtk_window_set_child, gtk_window_destroy, &
+               & gtk_radio_button_new, gtk_toggle_button_get_active, &
+               & gtk_widget_show, gtk_window_new, gtk_init
+  use g, only: g_main_loop_new, g_main_loop_run, g_main_loop_quit
 
   implicit none
-
   type(c_ptr) :: box, window, qbut, group
-  type(c_ptr),dimension(6) :: rbut
+  type(c_ptr), dimension(6) :: rbut
+  type(c_ptr) :: my_gmainloop
 
 contains
   subroutine my_destroy(widget, gdata) bind(c)
     type(c_ptr), value :: widget, gdata
 
     print *, "Exit called"
-    call gtk_main_quit ()
+    call g_main_loop_quit(my_gmainloop)
   end subroutine my_destroy
 
   subroutine rb_toggle(widget, gdata) bind(c)
     type(c_ptr), value :: widget, gdata
-
     integer(kind=c_int), pointer :: fdata
     integer(kind=c_int) :: sdata
 
@@ -67,11 +69,10 @@ contains
 end module rb_handlers
 
 program radio
-
   ! RADIO
   ! Demo of a radio button group
+  ! https://developer.gnome.org/gtk4/stable/GtkRadioButton.html
 
-  ! use gtk_hl
   use rb_handlers
 
   implicit none
@@ -79,20 +80,19 @@ program radio
   integer(kind=c_int), dimension(6), target :: isel=[ (i-1,i=1,6) ]
   character(len=10) :: label
 
-  ! Initialize GTK+
+  ! Initialize GTK
   call gtk_init()
 
   ! Create a window and a vertical box
   window = hl_gtk_window_new('radios'//c_null_char, destroy=c_funloc(my_destroy))
   box = hl_gtk_box_new(homogeneous=TRUE)
-  call gtk_container_add(window, box)
+  call gtk_window_set_child(window, box)
 
   ! make 6 radio buttons and put them into the box (the group is
   ! the list item that links the buttons together and is used for
   ! collective operations on the set of buttons. The group must be set to
-  ! a NULL pointer before entry otherwise the internals may get confused.
-
-  group=C_NULL_PTR
+  ! a NULL pointer before entry otherwise the internals may get confused.)
+  group = c_null_ptr
 
   do i=1,6
      write(label,"('Choice #',i0)") i-1
@@ -113,6 +113,7 @@ program radio
   call gtk_widget_show(window)
 
   ! Event loop
-  call gtk_main
+  my_gmainloop = g_main_loop_new(c_null_ptr, FALSE)
+  call g_main_loop_run(my_gmainloop)
 
 end program radio
