202f5170fb37b23d5025dc94ec5147452328ee2e
vmagnin <vincent.magnin@libertysurf.fr>
2020-06-20 15:51:10 +0200

    examples/gtkbuilder.f90: removed connect_signals functions and code clean-up. Now working with 3.98.4

--- examples/gtkbuilder.f90
+++ examples/gtkbuilder.f90
@@ -1,33 +1,34 @@
 ! Copyright (C) 2011
 ! Free Software Foundation, Inc.
-
+!
 ! This file is part of the gtk-fortran gtk+ Fortran Interface library.
-
+!
 ! This is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
 ! the Free Software Foundation; either version 3, or (at your option)
 ! any later version.
-
+!
 ! This software is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-
+!
 ! Under Section 7 of GPL version 3, you are granted additional
 ! permissions described in the GCC Runtime Library Exception, version
 ! 3.1, as published by the Free Software Foundation.
 ! You should have received a copy of the GNU General Public License along with
 ! this program; see the files COPYING3 and COPYING.RUNTIME respectively.
 ! If not, see <http://www.gnu.org/licenses/>.
-!
+!------------------------------------------------------------------------------
 ! Jens Hunger, 03-27-2011
-! Last modified: 04-01-2011
+! Last modified: vmagnin 2020-06-20 (GTK 4 version)
+!------------------------------------------------------------------------------
 
 module widgets
-  ! declares the used GTK widgets
   use iso_c_binding
-  implicit none
 
+  implicit none
+  ! declares the used GTK widgets
   type(c_ptr) :: window
   type(c_ptr) :: builder
   type(c_ptr) :: my_gmainloop
@@ -35,14 +36,14 @@ end module
 
 module handlers
   ! This module is just copied from gtkhello2.f90
-  use gtk, only: gtk_builder_add_from_file, gtk_builder_connect_signals, gtk_buil&
-  &der_connect_signals_full, gtk_builder_get_object, gtk_builder_new, &
-  & gtk_widget_show,&
-  &FALSE, c_null_char, c_null_ptr, gtk_init, g_signal_connect
+  use widgets
+
+  use gtk, only: gtk_builder_add_from_file, gtk_builder_get_object, &
+  & gtk_builder_new, gtk_widget_show, FALSE, gtk_init, g_signal_connect
+
   use g, only: g_object_unref, g_signal_connect_object, &
              & g_main_loop_new, g_main_loop_run, g_main_loop_quit
 
-  use widgets
   implicit none
 
 contains
@@ -52,43 +53,41 @@ contains
   ! Note that events are a special type of signals, coming from the
   ! X Window system. Then callback functions must have an event argument:
   function delete_event (widget, event, gdata) result(ret)  bind(c)
-    use iso_c_binding, only: c_ptr, c_int
     integer(c_int)    :: ret
-    type(c_ptr), value :: widget, event, gdata
+    type(c_ptr), value, intent(in) :: widget, event, gdata
+
     print *, "my delete_event"
     ret = FALSE
   end function delete_event
 
   ! "destroy" is a GtkObject signal
   subroutine destroy (widget, gdata) bind(c)
-    use iso_c_binding, only: c_ptr
-    type(c_ptr), value :: widget, gdata
+    type(c_ptr), value, intent(in) :: widget, gdata
+
     print *, "my destroy"
     call g_main_loop_quit (my_gmainloop)
   end subroutine destroy
 
   ! "clicked" is a GtkButton signal
   function hello (widget, gdata ) result(ret)  bind(c)
-    use iso_c_binding, only: c_ptr, c_int
     integer(c_int)    :: ret
-    type(c_ptr), value :: widget, gdata
+    type(c_ptr), value, intent(in) :: widget, gdata
+
     print *, "Hello World!"
     ret = FALSE
   end function hello
 
   function button1clicked (widget, gdata ) result(ret)  bind(c)
-    use iso_c_binding, only: c_ptr, c_int
     integer(c_int)    :: ret
-    type(c_ptr), value :: widget, gdata
+    type(c_ptr), value, intent(in) :: widget, gdata
+
     print *, "Button 1 clicked!"
     ret = FALSE
   end function button1clicked
 
   function button2clicked (widget, gdata ) result(ret)  bind(c)
-    use iso_c_binding, only: c_ptr, c_int
     integer(c_int)    :: ret
-    type(c_ptr), value :: widget, gdata
-
+    type(c_ptr), value, intent(in) :: widget, gdata
     integer, pointer :: val
 
     print *, "Button 2 clicked!"
@@ -99,14 +98,15 @@ contains
        val = val + 1
     end if
   end function button2clicked
-
 end module handlers
+
 
 module connect
 ! replacement for gtk_builder_connect_signals from GModule (see also gtkbuilder2.f90)
    use handlers
+
    implicit none
-   
+
    type handler
       character(kind=c_char,len=30) ::handler_name
       type(C_FUNPTR) :: handler_ptr
@@ -123,7 +123,7 @@ module connect
    logical::handlers_initialized=.false.
 
    contains
-   
+
 ! String routine from C_interface_module by Joseph M. Krahn
 ! http://fortranwiki.org/fortran/show/c_interface_module
 ! Copy a C string, passed as a char-array reference, to a Fortran string.
@@ -131,14 +131,17 @@ module connect
     character(len=1,kind=C_char), intent(in) :: C_string(*)
     character(len=*), intent(out) :: F_string
     integer :: i
+
     i=1
     do while(C_string(i)/=c_null_char .and. i<=len(F_string))
       F_string(i:i) = C_string(i)
       i=i+1
     end do
+
     if (i<len(F_string)) F_string(i:) = ' '
    end subroutine C_F_string_chars
-  
+
+
 !void        (*GtkBuilderConnectFunc)        (GtkBuilder *builder,
 !                                             GObject *object,
 !                                             const gchar *signal_name,
@@ -186,20 +189,17 @@ module connect
             exit
          endif
       enddo
-
    end subroutine connect_signals
-   
 end module connect
 
-program gtkbuilder
-  
-  use connect
-  
-  implicit none
 
+program gtkbuilder
+  use connect
+
+  implicit none
   integer(c_int) :: guint
   type(c_ptr) :: error
-  error=c_null_ptr
+  error = c_null_ptr
 
   ! Initialize the GTK Library
   call gtk_init ()
@@ -207,8 +207,10 @@ program gtkbuilder
   ! create a new GtkBuilder object
   builder = gtk_builder_new ()
 
-  ! parse the Glade3 XML file 'gtkbuilder.glade' and add its contents to the GtkBuilder object
-  guint = gtk_builder_add_from_file (builder, "gtkbuilder.glade"//c_null_char, error)
+  ! parse the Glade3 XML file 'gtkbuilder.glade' and add
+  ! its contents to the GtkBuilder object
+  guint = gtk_builder_add_from_file (builder, &
+                         & "gtkbuilder.glade"//c_null_char, error)
   if (guint == 0) then
      print *, "Could not open gtkbuilder.glade"
      stop
@@ -216,18 +218,15 @@ program gtkbuilder
 
   ! get a pointer to the GObject "window" from GtkBuilder
   window = gtk_builder_get_object (builder, "window"//c_null_char)
-  
-  ! connect signals to objects using the subroutine "connect_signals" in the module "connect"
-  call gtk_builder_connect_signals_full (builder, c_funloc(connect_signals), c_null_ptr)  
-     
-  ! free all memory used by XML stuff      
+
+  ! free all memory used by XML stuff
   call g_object_unref (builder)
   
-  ! Show the Application Window      
-  call gtk_widget_show (window)       
+  ! Show the Application Window
+  call gtk_widget_show (window)
   
   ! Enter the GTK Main Loop
   my_gmainloop = g_main_loop_new(c_null_ptr, FALSE)
   call g_main_loop_run(my_gmainloop)
-        
 end program gtkbuilder
+
