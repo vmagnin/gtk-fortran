104b5622de32e070ba98e1a729854d91ca0597ed
vmagnin <vincent.magnin@libertysurf.fr>
2020-07-15 10:10:04 +0200

    examples/hl_list_n.f90: now a GtkApplication

--- examples/hl_list_n.f90
+++ examples/hl_list_n.f90
@@ -22,7 +22,7 @@
 ! If not, see <http://www.gnu.org/licenses/>.
 !------------------------------------------------------------------------------
 ! Contributed by James Tappin.
-! Last modification: vmagnin 2020-06-12 (GTK 4)
+! Last modification: vmagnin 2020-06-12 (GTK 4), 2020-07-15
 !------------------------------------------------------------------------------
 
 module ln_handlers
@@ -31,24 +31,21 @@ module ln_handlers
   use gtk_hl_button
   use gtk_hl_tree
   use gtk_hl_entry
-  use gtk, only: gtk_button_new, gtk_window_set_child, &
-       & gtk_widget_show, gtk_window_new, gtk_init
-  use g, only: alloca, g_object_set_property, &
-             & g_main_loop_new, g_main_loop_run, g_main_loop_quit
+  use gtk, only: gtk_button_new, gtk_window_set_child, gtk_widget_show, &
+               & gtk_window_destroy
+  use g, only: alloca, g_object_set_property
 
   implicit none
   ! The widgets. (Strictly only those that need to be accessed
   ! by the handlers need to go here).
   type(c_ptr) :: ihwin,ihscrollcontain,ihlist, base, &
        &  qbut, lbl
-  type(c_ptr) :: my_gmainloop
 
 contains
   subroutine my_destroy(widget, gdata) bind(c)
     type(c_ptr), value, intent(in) :: widget, gdata
-
     print *, "Exit called"
-    call g_main_loop_quit (my_gmainloop)
+    call gtk_window_destroy(ihwin)
   end subroutine my_destroy
 
   subroutine list_select(list, gdata) bind(c)
@@ -155,102 +152,105 @@ contains
     end if
   end subroutine cell_edited
 
+  subroutine activate(app, gdata) bind(c)
+    use iso_c_binding, only: c_ptr, c_funloc, c_null_char
+    use gtk, only: gtk_application_window_new, gtk_window_set_title
+    implicit none
+    type(c_ptr), value, intent(in)  :: app, gdata
+    character(len=35) :: line
+    integer(kind=c_int) :: i, ltr
+    integer(kind=type_kind), dimension(8) :: ctypes
+    character(len=20), dimension(8) :: titles
+    integer(kind=c_int), dimension(8) :: sortable, editable
+    integer(kind=c_int), target :: fmt_col = 2
+    character(kind=c_char), dimension(10) :: codes
+
+    ! Create the window:
+    ihwin = gtk_application_window_new(app)
+    call gtk_window_set_title(ihwin, "multi-column list demo"//c_null_char)
+ 
+    ! Now make a column box & put it into the window
+    base = hl_gtk_box_new()
+    call gtk_window_set_child(ihwin, base)
+
+    ! Now make a multi column list with multiple selections enabled
+    ctypes = (/ G_TYPE_STRING, G_TYPE_INT, G_TYPE_INT, G_TYPE_FLOAT, &
+         & G_TYPE_UINT64, G_TYPE_BOOLEAN, G_TYPE_CHAR, G_TYPE_UCHAR /)
+    sortable = (/ FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE /)
+    editable = (/ TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE /)
+    codes = [ 'A', 'X', 'B', '?', 'A', 'C', char(185), 'u', '*', char(201)]
+
+    titles(1) = "Name"
+    titles(2) = "N"
+    titles(3) = "3N"
+    titles(4) = "Log(n)"
+    titles(5) = "N**4"
+    titles(6) = "Odd?"
+    titles(7) = "Code"
+    titles(8) = "Ucode"
+
+    ihlist = hl_gtk_listn_new(ihscrollcontain, types=ctypes, &
+         & changed=c_funloc(list_select),&
+         & multiple=TRUE, height=250_c_int, swidth=600_c_int, titles=titles, &
+         & sortable=sortable, editable=editable, &
+         & edited=c_funloc(cell_edited))
+
+    call hl_gtk_listn_set_cell_data_func(ihlist, fmt_col, &
+         & func=c_funloc(display_int), &
+         & data=c_loc(fmt_col))
+
+    ! Now put 10 rows into it
+    do i=1,10
+       call hl_gtk_listn_ins(ihlist)
+       write(line,"('List entry number ',I0)") i
+       ltr=len_trim(line)+1
+       line(ltr:ltr)=c_null_char
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 0_c_int, svalue=line)
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 1_c_int, ivalue=i)
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 2_c_int, ivalue=3_c_int*i)
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 3_c_int, &
+            & fvalue=log10(real(i)))
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 4_c_int, &
+            & l64value=int(i,c_int64_t)**4)
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 5_c_int, &
+            & ivalue=mod(i,2_c_int))
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 6_c_int, svalue=codes(i))
+       call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 7_c_int, svalue=codes(i))
+    end do
+
+    ! A silly idea to test a hunch
+    print *, codes
+    codes = char(ichar(codes)-2)
+    print *, codes
+
+    ! It is the scrollcontainer that is placed into the box.
+    call hl_gtk_box_pack(base, ihscrollcontain)
+
+    ! Add a note about editable columns
+    lbl = gtk_label_new("The ""Name"" and ""N"" columns are editable"//c_null_char)
+    call hl_gtk_box_pack(base, lbl)
+
+    ! Also a quit button
+    qbut = hl_gtk_button_new("Quit"//c_null_char, clicked=c_funloc(my_destroy))
+    call hl_gtk_box_pack(base,qbut)
+
+    ! realize the window
+    print *, "Created"
+    call gtk_widget_show(ihwin)
+    print *, "Realized"
+    end subroutine activate
 end module ln_handlers
+
 
 program list_n
   ! LIST_N
   ! Demo of multi column list
-
+  use iso_c_binding, only: c_ptr, c_funloc, c_null_char
   use ln_handlers
 
   implicit none
-  character(len=35) :: line
-  integer(kind=c_int) :: i, ltr
-  integer(kind=type_kind), dimension(8) :: ctypes
-  character(len=20), dimension(8) :: titles
-  integer(kind=c_int), dimension(8) :: sortable, editable
-  integer(kind=c_int), target :: fmt_col = 2
-  character(kind=c_char), dimension(10) :: codes
+  type(c_ptr)        :: app
 
-  ! Initialize GTK
-  call gtk_init()
-
-  ! Create a window that will hold the widget system
-  ihwin=hl_gtk_window_new('multi-column list demo'//c_null_char, &
-       & destroy=c_funloc(my_destroy))
-
-  ! Now make a column box & put it into the window
-  base = hl_gtk_box_new()
-  call gtk_window_set_child(ihwin, base)
-
-  ! Now make a multi column list with multiple selections enabled
-  ctypes = (/ G_TYPE_STRING, G_TYPE_INT, G_TYPE_INT, G_TYPE_FLOAT, &
-       & G_TYPE_UINT64, G_TYPE_BOOLEAN, G_TYPE_CHAR, G_TYPE_UCHAR /)
-  sortable = (/ FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE /)
-  editable = (/ TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE /)
-  codes = [ 'A', 'X', 'B', '?', 'A', 'C', char(185), 'u', '*', char(201)]
-
-  titles(1) = "Name"
-  titles(2) = "N"
-  titles(3) = "3N"
-  titles(4) = "Log(n)"
-  titles(5) = "N**4"
-  titles(6) = "Odd?"
-  titles(7) = "Code"
-  titles(8) = "Ucode"
-
-  ihlist = hl_gtk_listn_new(ihscrollcontain, types=ctypes, &
-       & changed=c_funloc(list_select),&
-       & multiple=TRUE, height=250_c_int, swidth=600_c_int, titles=titles, &
-       & sortable=sortable, editable=editable, &
-       & edited=c_funloc(cell_edited))
-
-  call hl_gtk_listn_set_cell_data_func(ihlist, fmt_col, &
-       & func=c_funloc(display_int), &
-       & data=c_loc(fmt_col))
-
-  ! Now put 10 rows into it
-  do i=1,10
-     call hl_gtk_listn_ins(ihlist)
-     write(line,"('List entry number ',I0)") i
-     ltr=len_trim(line)+1
-     line(ltr:ltr)=c_null_char
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 0_c_int, svalue=line)
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 1_c_int, ivalue=i)
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 2_c_int, ivalue=3_c_int*i)
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 3_c_int, &
-          & fvalue=log10(real(i)))
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 4_c_int, &
-          & l64value=int(i,c_int64_t)**4)
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 5_c_int, &
-          & ivalue=mod(i,2_c_int))
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 6_c_int, svalue=codes(i))
-     call hl_gtk_listn_set_cell(ihlist, i-1_c_int, 7_c_int, svalue=codes(i))
-  end do
-
-  ! A silly idea to test a hunch
-  print *, codes
-  codes = char(ichar(codes)-2)
-  print *, codes
-
-  ! It is the scrollcontainer that is placed into the box.
-  call hl_gtk_box_pack(base, ihscrollcontain)
-
-  ! Add a note about editable columns
-  lbl = gtk_label_new("The ""Name"" and ""N"" columns are editable"//c_null_char)
-  call hl_gtk_box_pack(base, lbl)
-
-  ! Also a quit button
-  qbut = hl_gtk_button_new("Quit"//c_null_char, clicked=c_funloc(my_destroy))
-  call hl_gtk_box_pack(base,qbut)
-
-  ! realize the window
-  print *, "Created"
-  call gtk_widget_show(ihwin)
-  print *, "Realized"
-
-  ! Event loop
-  my_gmainloop = g_main_loop_new(c_null_ptr, FALSE)
-  call g_main_loop_run(my_gmainloop)
-
+  app = hl_gtk_application_new("gtk-fortran.examples.hl_list_n"//c_null_char, &
+                             & c_funloc(activate))
 end program list_n
