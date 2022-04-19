87a31a5b5ad71c28d8201413df075523bd8444f1
vmagnin <vincent.magnin@libertysurf.fr>
2022-04-19 11:14:18 +0200

    Adds tutorials directory (GTK 4 version)

---
+++ tutorials/my_first_gtk_app1.f90
@@ -0,0 +1,19 @@
+! $ gfortran -Wall -Wextra -std=f2008 -pedantic -g my_first_gtk_app1.f90 $(pkg-config --cflags --libs gtk-4-fortran)
+
+module handlers
+  use, intrinsic :: iso_c_binding
+  use gtk, only:
+  use g, only:
+
+  implicit none
+
+  contains
+
+end module handlers
+
+program my_first_gtk_app
+  use handlers
+
+  implicit none
+
+end program
---
+++ tutorials/my_first_gtk_app2.f90
@@ -0,0 +1,25 @@
+! $ gfortran -Wall -Wextra -std=f2008 -pedantic -g my_first_gtk_app2.f90 $(pkg-config --cflags --libs gtk-4-fortran)
+
+module handlers
+  use, intrinsic :: iso_c_binding
+  use gtk, only: gtk_application_new, G_APPLICATION_FLAGS_NONE
+  use g, only: g_application_run, g_object_unref
+
+  implicit none
+
+  contains
+
+end module handlers
+
+program my_first_gtk_app
+  use handlers
+
+  implicit none
+  type(c_ptr)    :: app
+  integer(c_int) :: status
+
+  app = gtk_application_new("gtk-fortran.my_first_gtk_app"//c_null_char, &
+                          & G_APPLICATION_FLAGS_NONE)
+  status = g_application_run(app, 0_c_int, [c_null_ptr])
+  call g_object_unref(app)
+end program
