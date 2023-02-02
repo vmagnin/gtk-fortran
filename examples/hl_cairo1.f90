6d29ed8d04641498d4c7765f4581800eb9641db2
vmagnin <vincent.magnin@libertysurf.fr>
2023-02-02 10:16:54 +0100

    Fixed the names of the licenses files in the files headers

--- examples/hl_cairo1.f90
+++ examples/hl_cairo1.f90
@@ -18,7 +18,7 @@
 ! 3.1, as published by the Free Software Foundation.
 !
 ! You should have received a copy of the GNU General Public License along with
-! this program; see the files COPYING3 and COPYING.RUNTIME respectively.
+! this program; see the files LICENSE and LICENSE_EXCEPTION respectively.
 ! If not, see <http://www.gnu.org/licenses/>.
 !------------------------------------------------------------------------------
 ! Contributed by James Tappin,
