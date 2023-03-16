f8ccb743c3da3497e3ceaa1b49103b09ea83441c
vmagnin <vincent.magnin@libertysurf.fr>
2023-03-16 14:35:45 +0100

    src/cfwrapper/analyze.py: refactoring, added an analyze_arguments() function

--- src/cfwrapper/analyze.py
+++ src/cfwrapper/analyze.py
@@ -82,8 +82,6 @@ def split_prototype(prototype):
     return function_type, f_name, args
 
 
-# TODO: these procedures need refactoring !
-
 def analyze_prototypes(index, module_name, f_file_name, f_file, preprocessed_list,
                        whole_file_original, c_dir, c_file_name, gtk_enums,
                        gtk_funptr, TYPES_DICT, TYPES2_DICT, my_stats, my_errors,
@@ -152,65 +150,9 @@ def analyze_prototypes(index, module_name, f_file_name, f_file, preprocessed_lis
         else:
             function_status = ""
 
-        # Each argument of the function is analyzed:
-        error_flag = False
-        declarations = ""
-        args_list = ""
-        for arg in args:
-            if arg == "void":
-                continue    # Next argument in the list
-
-            # Can we find the type of the argument ? The var_type variable is
-            # not used elsewhere, but this test is compulsory. Do not remove.
-            try:
-                var_type = RGX_VAR_TYPE.search(arg).group(1)
-            except AttributeError:
-                my_errors.new_error(c_dir, c_file_name,
-                                    "Variable type not found", proto, True)
-                continue    # Next argument in the list
-
-            # Corresponding Fortran type of the argument:
-            f_type, iso_c = iso_c_binding(arg, False, gtk_enums, gtk_funptr,
-                                          TYPES_DICT, TYPES2_DICT)
-            if iso_c not in my_stats.used_types:
-                my_stats.append_type(iso_c)
-
-            if "c_" in f_type:
-                # Determine iso_c type to use:
-                if f_use == "":
-                    f_use = iso_c
-                else:
-                    # Verify that each iso_c appear only once:
-                    RGX_ISO_C = re.compile("("+iso_c+")"+r"([^\w]|$)")
-                    if RGX_ISO_C.search(f_use) is None:
-                        f_use += ", " + iso_c
-            elif "?" in f_type:     # Type not found
-                error_flag = True
-                my_errors.new_error(c_dir, c_file_name, "Unknown type:  " + arg,
-                                    proto, True)
-
-            # Search the variable name:
-            try:
-                var_name = RGX_VAR_NAME.search(arg).group(1)
-            except AttributeError:
-                my_errors.new_error(c_dir, c_file_name,
-                                    "Variable name not found", proto, False)
-                continue    # Next argument in the list
-
-            # Unknown dimension arrays are passed by address, others by value:
-            if "(*)" in f_type:
-                passvar = ""
-            else:
-                passvar = ", value"
-
-            # Add this variable:
-            if args_list == "":
-                args_list = var_name
-            else:
-                args_list += ", " + var_name
-
-            declarations += 1*TAB + f_type + passvar + " :: " + var_name + "\n"
-
+        # The list `args` of the function arguments is now analyzed:
+        args_list, f_use, declarations, error_flag = analyze_arguments(args, f_use, proto, c_dir, c_file_name, gtk_enums,
+                                                                       gtk_funptr, TYPES_DICT, TYPES2_DICT, my_stats, my_errors)
         # Write the Fortran interface in the .f90 file:
         if not error_flag:
             write_fortran_interface(index, module_name, f_file_name, f_file,
@@ -218,6 +160,76 @@ def analyze_prototypes(index, module_name, f_file_name, f_file, preprocessed_lis
                                     function_status, proto, f_procedure, f_name,
                                     args_list, f_use, declarations, isfunction,
                                     returned_type, f_the_end, my_stats)
+
+
+
+def analyze_arguments(args, f_use, proto, c_dir, c_file_name, gtk_enums, gtk_funptr, TYPES_DICT, TYPES2_DICT, my_stats, my_errors):
+    """This function analyzes the list of arguments `args` of the C function.
+       It returns the list of Fortran arguments, the iso_c_binding types needed,
+       the Fortran declarations of the arguments and an error_flag.
+    """
+    error_flag = False
+    declarations = ""
+    args_list = ""
+
+    for arg in args:
+        if arg == "void":
+            continue    # Next argument in the list
+
+        # Can we find the type of the argument ? The var_type variable is
+        # not used elsewhere, but this test is compulsory. Do not remove.
+        try:
+            var_type = RGX_VAR_TYPE.search(arg).group(1)
+        except AttributeError:
+            error_flag = True
+            my_errors.new_error(c_dir, c_file_name,
+                                "Variable type not found", proto, True)
+            continue    # Next argument in the list
+
+        # Corresponding Fortran type of the argument:
+        f_type, iso_c = iso_c_binding(arg, False, gtk_enums, gtk_funptr,
+                                      TYPES_DICT, TYPES2_DICT)
+        if iso_c not in my_stats.used_types:
+            my_stats.append_type(iso_c)
+
+        if "c_" in f_type:
+            # Determine iso_c type to use:
+            if f_use == "":
+                f_use = iso_c
+            else:
+                # Verify that each iso_c appears only once:
+                RGX_ISO_C = re.compile("("+iso_c+")"+r"([^\w]|$)")
+                if RGX_ISO_C.search(f_use) is None:
+                    f_use += ", " + iso_c
+        elif "?" in f_type:
+            error_flag = True
+            my_errors.new_error(c_dir, c_file_name, "Unknown type:  " + arg,
+                                proto, True)
+
+        # Search the variable name:
+        try:
+            var_name = RGX_VAR_NAME.search(arg).group(1)
+        except AttributeError:
+            error_flag = True
+            my_errors.new_error(c_dir, c_file_name,
+                                "Variable name not found", proto, False)
+            continue    # Next argument in the list
+
+        # Unknown dimension arrays are passed by address, others by value:
+        if "(*)" in f_type:
+            passvar = ""
+        else:
+            passvar = ", value"
+
+        # Add this variable:
+        if args_list == "":
+            args_list = var_name
+        else:
+            args_list += ", " + var_name
+
+        declarations += 1*TAB + f_type + passvar + " :: " + var_name + "\n"
+
+    return args_list, f_use, declarations, error_flag
 
 
 def write_fortran_interface(index, module_name, f_file_name, f_file,
