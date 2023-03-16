7f6716c62541c70bf3ab56e840abc44b773bbce2
vmagnin <vincent.magnin@libertysurf.fr>
2023-03-16 16:20:35 +0100

    src/cfwrapper/cleaning.py: remove G_ANALYZER_NORETURN macro at the end of prototypes

--- src/cfwrapper/cleaning.py
+++ src/cfwrapper/cleaning.py
@@ -93,6 +93,7 @@ def clean_header_file(c_file_name, whole_file, enums_file):
     whole_file = re.sub("(?m)^(G_DECLARE_INTERFACE|G_DECLARE_DERIVABLE_TYPE).*$", "", whole_file)
     # Remove GNU macros at the end of declarations (functions prototypes):
     whole_file = re.sub(r"G_GNUC_[\w (),]*;", ";", whole_file)
+    whole_file = re.sub(r"G_ANALYZER_NORETURN *;", ";", whole_file)
     # Remove some GNU macros at the beginning of functions prototypes:
     whole_file = re.sub(r"G_DECLARE_FINAL_TYPE ?\(.*?\)", "", whole_file)
     whole_file = re.sub(r"G_DEFINE_AUTOPTR_CLEANUP_FUNC ?\(.*?\)", "", whole_file)
