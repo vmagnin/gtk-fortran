d89d7fdd1d67e0d053dcf1460afccfd926295b2d
vmagnin <vincent.magnin@libertysurf.fr>
2025-02-19 18:38:43 +0100

    cfwrapper/fortran.py: added comments

--- src/cfwrapper/fortran.py
+++ src/cfwrapper/fortran.py
@@ -23,50 +23,56 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 # Contributed by Vincent Magnin, 01.28.2011
-# Last modification: 2024-10-23
+# Last modification: 2025-02-19
 
-""" This module contains functions used in the cfwrapper.
+""" This module contains a function to translate C types to Fortran types.
 """
 
 import re           # Regular expression library
 
 from scan_types_and_enums import types_enums
 
-
-# Used to identify a C type:
+# The regex used to identify a C type:
 RGX_TYPE = re.compile(r"^ *((const )?\w+)[ \*]?")
-
+# beginning by zero or several spaces,
+# possibly by a "const " statement,
+# followed by the name of the type,
+# and possibly ending by a space or a star.
 
 def iso_c_binding(declaration, isReturned):
     """ Returns the Fortran type corresponding to a C type in the ISO_C_BINDING
-        module (limited to C types used in GTK), and the KIND type.
-        The declaration contains the type and the name of the entity.
-        The isReturned flag distinguishes types returned by a function and
-        arguments types.
+        module (limited to C types used in GTK), and the KIND type,
+        for example 'integer(c_int)', 'c_int'.
+        The declaration contains the type and the name of the entity,
+        for example 'GVariant *value'
+        The isReturned flag argument distinguishes types returned by a 
+        function (true) or arguments types (false).
     """
+    
+    # Try to find a C type in the declaration:
     try:
         c_type = RGX_TYPE.search(declaration).group(1)
     except AttributeError:
-        return "?", "?"    # error
+        return "?", "?"    # No C type found => error
 
-    # Remove "const " statement:
+    # Remove a possible "const " statement:
     declaration = re.sub(r"^(const )", "", declaration)
 
-    # Is it a "typedef enum" ?
+    # Is it a "typedef enum"?
     for item in types_enums.gtk_enums:
         if item in c_type:
             return "integer(c_int)", "c_int"
 
-    # Is it a pointer toward a function ?
+    # Is it a pointer toward a function?
     for item in types_enums.gtk_funptr:
         if item in c_type:
             return "type(c_funptr)", "c_funptr"
 
-    # typedef void* gpointer;
+    # Is it a gpointer? (an untyped pointer equivalent to void*)
     if ("gpointer" in c_type) or ("gconstpointer" in c_type):
         return "type(c_ptr)", "c_ptr"
 
-    # Is it a pointer ?
+    # Is it a C pointer? (one or more stars)
     if "*" in declaration:
         # GVariant and GVariantType are structures, generally used via pointers
         if ("GVariant" in c_type):
@@ -74,32 +80,37 @@ def iso_c_binding(declaration, isReturned):
                 return "type(c_ptr), dimension(*)", "c_ptr"
             else:
                 return "type(c_ptr)", "c_ptr"
-        # Is it a string (char or gchar array) ?
+        # Is it a string (char or gchar array) or a list of strings?
         elif ("char" in c_type) and (not isReturned):
             if declaration.count('*') >= 2:
                 # An array of C strings:
                 return "type(c_ptr), dimension(*)", "c_ptr"
             else:
                 return "character(kind=c_char), dimension(*)", "c_char"
+        # Other cases:
         else:
             return "type(c_ptr)", "c_ptr"
 
-    # Is it an array ?
+    # Is it an array?
     if "[" in declaration:
         array = ", dimension(*)"
     else:
         array = ""
 
-    # Other cases:
+    # Other cases defined in the TYPES dictionaries:
     if len(declaration.split()) >= 3:  # Two words type + the name of the entity
+        # The TYPES2_DICT contains a list of two words type
         for item in types_enums.TYPES2_DICT:
             # A Python set is an unordered collection of distinct hashable objects
             if set(item.split()).issubset(set(declaration.split())):
                 return types_enums.TYPES2_DICT[item][0] + array, types_enums.TYPES2_DICT[item][1]
-    else:  # It is a one word type
+    else:  # It is therefore a one word type
         for item in types_enums.TYPES_DICT:
             if item in c_type.split():
                 return types_enums.TYPES_DICT[item][0] + array, types_enums.TYPES_DICT[item][1]
 
-    # It is finally an unknown type:
+    # We failed to identify the C type.
+    # This print can be used for debugging those cases:
+    # print(declaration)
     return "?", "?"
+    
